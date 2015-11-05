b95ccd06a791066f19add26cffd4474414141c48
Martin Sefcik <mail@martinsefcik.sk>
2015-11-05 16:48:36 +0100

    Removed testify and used standard Go testing approach

--- projects_test.go
+++ projects_test.go
@@ -1,32 +1,51 @@
 package gitlab
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestListAllProjects(t *testing.T) {
 	ts, client := Stub("stubs/projects/index.json")
+	defer ts.Close()
 
 	opt := &ListProjectsOptions{}
 	projects, _, err := client.Projects.ListAllProjects(opt)
 
-	assert.Equal(t, err, nil)
-	assert.Equal(t, len(projects), 2)
-	assert.Equal(t, projects[0].Name, "project")
-	assert.Equal(t, projects[1].Name, "project2")
-	defer ts.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(projects) != 2 {
+		t.Fail()
+	}
+	if projects[0].Name != "project" {
+		t.Fail()
+	}
+	if projects[1].Name != "project2" {
+		t.Fail()
+	}
 }
 
 func TestGetProject(t *testing.T) {
 	ts, client := Stub("stubs/projects/show.json")
+	defer ts.Close()
 
 	project, _, err := client.Projects.GetProject(1)
 
-	assert.Equal(t, err, nil)
-	assert.Equal(t, project.Name, "project")
-	assert.Equal(t, project.Namespace.Name, "group")
-	assert.Equal(t, project.Permissions.ProjectAccess.AccessLevel, MasterPermissions)
-	assert.Nil(t, project.Permissions.GroupAccess)
-	defer ts.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if project.Name != "project" {
+		t.Fail()
+	}
+	if project.Namespace.Name != "group" {
+		t.Fail()
+	}
+	if project.Permissions.ProjectAccess.AccessLevel != MasterPermissions {
+		t.Fail()
+	}
+	if project.Permissions.GroupAccess != nil {
+		t.Fail()
+	}
 }
