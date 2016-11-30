4c0772a60f6142b70977dd8078c753b84a2fd2c1
Mustafa Yildirim <mustafayildirim@MacBook-Pro.local>
2016-11-30 12:05:11 +0300

    PipelineEvent type changed

--- events.go
+++ events.go
@@ -16,6 +16,8 @@
 
 package gitlab
 
+import "time"
+
 // PushEvent represents a push event.
 //
 // GitLab API docs:
@@ -312,8 +314,52 @@ type PipelineEvent struct {
 		FinishedAt string   `json:"finished_at"`
 		Duration   int      `json:"duration"`
 	} `json:"object_attributes"`
-	User    *User    `json:"user"`
-	Project *Project `json:"project"`
-	Commit  *Commit  `json:"commit"`
-	Builds  []*Build `json:"builds"`
+	User struct {
+		Name      string `json:"name"`
+		Username  string `json:"username"`
+		AvatarURL string `json:"avatar_url"`
+	} `json:"user"`
+	Project struct {
+		Name              string      `json:"name"`
+		Description       string      `json:"description"`
+		WebURL            string      `json:"web_url"`
+		AvatarURL         interface{} `json:"avatar_url"`
+		GitSSHURL         string      `json:"git_ssh_url"`
+		GitHTTPURL        string      `json:"git_http_url"`
+		Namespace         string      `json:"namespace"`
+		VisibilityLevel   int         `json:"visibility_level"`
+		PathWithNamespace string      `json:"path_with_namespace"`
+		DefaultBranch     string      `json:"default_branch"`
+	} `json:"project"`
+	Commit struct {
+		ID        string    `json:"id"`
+		Message   string    `json:"message"`
+		Timestamp time.Time `json:"timestamp"`
+		URL       string    `json:"url"`
+		Author    struct {
+			Name  string `json:"name"`
+			Email string `json:"email"`
+		} `json:"author"`
+	} `json:"commit"`
+	Builds []struct {
+		ID         int    `json:"id"`
+		Stage      string `json:"stage"`
+		Name       string `json:"name"`
+		Status     string `json:"status"`
+		CreatedAt  string `json:"created_at"`
+		StartedAt  string `json:"started_at"`
+		FinishedAt string `json:"finished_at"`
+		When       string `json:"when"`
+		Manual     bool   `json:"manual"`
+		User       struct {
+			Name      string `json:"name"`
+			Username  string `json:"username"`
+			AvatarURL string `json:"avatar_url"`
+		} `json:"user"`
+		Runner        string `json:"runner"`
+		ArtifactsFile struct {
+			Filename string `json:"filename"`
+			Size     string `json:"size"`
+		} `json:"artifacts_file"`
+	} `json:"builds"`
 }
