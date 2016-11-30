e77b5acacbe6bedc3095d14bc4d2d4038dc3640a
Mustafa Yildirim <mustafayildirim@MacBook-Pro.local>
2016-11-30 12:19:33 +0300

    Added BuildEvent

--- events.go
+++ events.go
@@ -363,3 +363,49 @@ type PipelineEvent struct {
 		} `json:"artifacts_file"`
 	} `json:"builds"`
 }
+
+//BuildEvent represents a build event
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/web_hooks/web_hooks.html#build-events
+type BuildEvent struct {
+	ObjectKind        string `json:"object_kind"`
+	Ref               string `json:"ref"`
+	Tag               bool   `json:"tag"`
+	BeforeSha         string `json:"before_sha"`
+	Sha               string `json:"sha"`
+	BuildID           int    `json:"build_id"`
+	BuildName         string `json:"build_name"`
+	BuildStage        string `json:"build_stage"`
+	BuildStatus       string `json:"build_status"`
+	BuildStartedAt    string `json:"build_started_at"`
+	BuildFinishedAt   string `json:"build_finished_at"`
+	BuildDuration     string `json:"build_duration"`
+	BuildAllowFailure bool   `json:"build_allow_failure"`
+	ProjectID         int    `json:"project_id"`
+	ProjectName       string `json:"project_name"`
+	User              struct {
+		ID    int    `json:"id"`
+		Name  string `json:"name"`
+		Email string `json:"email"`
+	} `json:"user"`
+	Commit struct {
+		ID          int    `json:"id"`
+		Sha         string `json:"sha"`
+		Message     string `json:"message"`
+		AuthorName  string `json:"author_name"`
+		AuthorEmail string `json:"author_email"`
+		Status      string `json:"status"`
+		Duration    string `json:"duration"`
+		StartedAt   string `json:"started_at"`
+		FinishedAt  string `json:"finished_at"`
+	} `json:"commit"`
+	Repository struct {
+		Name            string `json:"name"`
+		GitSSHURL       string `json:"git_ssh_url"`
+		Description     string `json:"description"`
+		Homepage        string `json:"homepage"`
+		GitHTTPURL      string `json:"git_http_url"`
+		VisibilityLevel int    `json:"visibility_level"`
+	} `json:"repository"`
+}
