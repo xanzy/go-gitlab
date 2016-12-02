0905e0f4df404e1ef1ee12853352ee93d2f40f21
Mustafa Yildirim <mustafayildirim@MacBook-Pro.local>
2016-12-02 15:37:27 +0300

    Remove GroupMergeEvent and fix MergeEvent for Group Webhooks

--- events.go
+++ events.go
@@ -236,38 +236,82 @@ type SnippetCommentEvent struct {
 // GitLab API docs:
 // https://docs.gitlab.com/ce/web_hooks/web_hooks.html#merge-request-events
 type MergeEvent struct {
-	ObjectKind       string `json:"object_kind"`
-	User             *User  `json:"user"`
+	ObjectKind string `json:"object_kind"`
+	User       *User  `json:"user"`
+	Project    struct {
+		Name              string               `json:"name"`
+		Description       string               `json:"description"`
+		WebURL            string               `json:"web_url"`
+		AvatarURL         string               `json:"avatar_url"`
+		GitSSHURL         string               `json:"git_ssh_url"`
+		GitHTTPURL        string               `json:"git_http_url"`
+		Namespace         string               `json:"namespace"`
+		VisibilityLevel   VisibilityLevelValue `json:"visibility_level"`
+		PathWithNamespace string               `json:"path_with_namespace"`
+		DefaultBranch     string               `json:"default_branch"`
+		Homepage          string               `json:"homepage"`
+		URL               string               `json:"url"`
+		SSHURL            string               `json:"ssh_url"`
+		HTTPURL           string               `json:"http_url"`
+	} `json:"project"`
 	ObjectAttributes struct {
-		ID              int         `json:"id"`
-		TargetBranch    string      `json:"target_branch"`
-		SourceBranch    string      `json:"source_branch"`
-		SourceProjectID int         `json:"source_project_id"`
-		AuthorID        int         `json:"author_id"`
-		AssigneeID      int         `json:"assignee_id"`
-		Title           string      `json:"title"`
-		CreatedAt       string      `json:"created_at"` // Should be *time.Time (see Gitlab issue #21468)
-		UpdatedAt       string      `json:"updated_at"` // Should be *time.Time (see Gitlab issue #21468)
-		StCommits       []*Commit   `json:"st_commits"`
-		StDiffs         []*Diff     `json:"st_diffs"`
-		MilestoneID     int         `json:"milestone_id"`
-		State           string      `json:"state"`
-		MergeStatus     string      `json:"merge_status"`
-		TargetProjectID int         `json:"target_project_id"`
-		Iid             int         `json:"iid"`
-		Description     string      `json:"description"`
-		Source          *Repository `json:"source"`
-		Target          *Repository `json:"target"`
-		LastCommit      *Commit     `json:"last_commit"`
-		WorkInProgress  bool        `json:"work_in_progress"`
-		URL             string      `json:"url"`
-		Action          string      `json:"action"`
-		Assignee        struct {
+		ID              int       `json:"id"`
+		TargetBranch    string    `json:"target_branch"`
+		SourceBranch    string    `json:"source_branch"`
+		SourceProjectID int       `json:"source_project_id"`
+		AuthorID        int       `json:"author_id"`
+		AssigneeID      int       `json:"assignee_id"`
+		Title           string    `json:"title"`
+		CreatedAt       string    `json:"created_at"` // Should be *time.Time (see Gitlab issue #21468)
+		UpdatedAt       string    `json:"updated_at"` // Should be *time.Time (see Gitlab issue #21468)
+		StCommits       []*Commit `json:"st_commits"`
+		StDiffs         []*Diff   `json:"st_diffs"`
+		MilestoneID     int       `json:"milestone_id"`
+		State           string    `json:"state"`
+		MergeStatus     string    `json:"merge_status"`
+		TargetProjectID int       `json:"target_project_id"`
+		Iid             int       `json:"iid"`
+		Description     string    `json:"description"`
+		Position        int       `json:"position"`
+		LockedAt        string    `json:"locked_at"`
+		UpdatedByID     int       `json:"updated_by_id"`
+		MergeError      string    `json:"merge_error"`
+		MergeParams     struct {
+			ForceRemoveSourceBranch string `json:"force_remove_source_branch"`
+		} `json:"merge_params"`
+		MergeWhenBuildSucceeds   bool        `json:"merge_when_build_succeeds"`
+		MergeUserID              int         `json:"merge_user_id"`
+		MergeCommitSha           string      `json:"merge_commit_sha"`
+		DeletedAt                string      `json:"deleted_at"`
+		ApprovalsBeforeMerge     string      `json:"approvals_before_merge"`
+		RebaseCommitSha          string      `json:"rebase_commit_sha"`
+		InProgressMergeCommitSha string      `json:"in_progress_merge_commit_sha"`
+		LockVersion              int         `json:"lock_version"`
+		TimeEstimate             int         `json:"time_estimate"`
+		Source                   *Repository `json:"source"`
+		Target                   *Repository `json:"target"`
+		LastCommit               struct {
+			ID        string     `json:"id"`
+			Message   string     `json:"message"`
+			Timestamp *time.Time `json:"timestamp"`
+			URL       string     `json:"url"`
+			Author    *Author    `json:"author"`
+		} `json:"last_commit"`
+		WorkInProgress bool   `json:"work_in_progress"`
+		URL            string `json:"url"`
+		Action         string `json:"action"`
+		Assignee       struct {
 			Name      string `json:"name"`
 			Username  string `json:"username"`
 			AvatarURL string `json:"avatar_url"`
 		} `json:"assignee"`
 	} `json:"object_attributes"`
+	Repository *Repository `json:"repository"`
+	Assignee   struct {
+		Name      string `json:"name"`
+		Username  string `json:"username"`
+		AvatarURL string `json:"avatar_url"`
+	} `json:"assignee"`
 }
 
 // WikiPageEvent represents a wiki page event.
--- events_test.go
+++ events_test.go
@@ -184,6 +184,13 @@ func TestMergeEventUnmarshal(t *testing.T) {
 		t.Errorf("ObjectAttributes.ID is %v, want %v", event.ObjectAttributes.ID, 99)
 	}
 
+	if event.ObjectAttributes.Source.Homepage != "http://example.com/awesome_space/awesome_project" {
+		t.Errorf("ObjectAttributes.Source.Homepage is %v, want %v", event.ObjectAttributes.Source.Homepage, "http://example.com/awesome_space/awesome_project")
+	}
+
+	if event.ObjectAttributes.LastCommit.ID != "da1560886d4f094c3e6c9ef40349f7d38b5d27d7" {
+		t.Errorf("ObjectAttributes.LastCommit.ID is %v, want %s", event.ObjectAttributes.LastCommit.ID, "da1560886d4f094c3e6c9ef40349f7d38b5d27d7")
+	}
 	if event.ObjectAttributes.Assignee.Name != "User1" {
 		t.Errorf("Assignee.Name is %v, want %v", event.ObjectAttributes.ID, "User1")
 	}
@@ -422,5 +429,165 @@ func TestBuildEventUnmarshal(t *testing.T) {
 
 	if event.BuildID != 1977 {
 		t.Errorf("BuildID is %v, want %v", event.BuildID, 1977)
+	}
+}
+
+func TestMergeEventUnmarshalFromGroup(t *testing.T) {
+
+	jsonObject := `{
+	"object_kind": "merge_request",
+	"user": {
+		"name": "Administrator",
+		"username": "root",
+		"avatar_url": "http://www.gravatar.com/avatar/d22738dc40839e3d95fca77ca3eac067?s=80\u0026d=identicon"
+	},
+	"project": {
+		"name": "example-project",
+		"description": "",
+		"web_url": "http://example.com/exm-namespace/example-project",
+		"avatar_url": null,
+		"git_ssh_url": "git@example.com:exm-namespace/example-project.git",
+		"git_http_url": "http://example.com/exm-namespace/example-project.git",
+		"namespace": "exm-namespace",
+		"visibility_level": 0,
+		"path_with_namespace": "exm-namespace/example-project",
+		"default_branch": "master",
+		"homepage": "http://example.com/exm-namespace/example-project",
+		"url": "git@example.com:exm-namespace/example-project.git",
+		"ssh_url": "git@example.com:exm-namespace/example-project.git",
+		"http_url": "http://example.com/exm-namespace/example-project.git"
+	},
+	"object_attributes": {
+		"id": 15917,
+		"target_branch ": "master ",
+		"source_branch ": "source-branch-test ",
+		"source_project_id ": 87,
+		"author_id ": 15,
+		"assignee_id ": 29,
+		"title ": "source-branch-test ",
+		"created_at ": "2016 - 12 - 01 13: 11: 10 UTC ",
+		"updated_at ": "2016 - 12 - 01 13: 21: 20 UTC ",
+		"milestone_id ": null,
+		"state ": "merged ",
+		"merge_status ": "can_be_merged ",
+		"target_project_id ": 87,
+		"iid ": 1402,
+		"description ": "word doc support for e - ticket ",
+		"position ": 0,
+		"locked_at ": null,
+		"updated_by_id ": null,
+		"merge_error ": null,
+		"merge_params": {
+			"force_remove_source_branch": "0"
+		},
+		"merge_when_build_succeeds": false,
+		"merge_user_id": null,
+		"merge_commit_sha": "ac3ca1559bc39abf963586372eff7f8fdded646e",
+		"deleted_at": null,
+		"approvals_before_merge": null,
+		"rebase_commit_sha": null,
+		"in_progress_merge_commit_sha": null,
+		"lock_version": 0,
+		"time_estimate": 0,
+		"source": {
+			"name": "example-project",
+			"description": "",
+			"web_url": "http://example.com/exm-namespace/example-project",
+			"avatar_url": null,
+			"git_ssh_url": "git@example.com:exm-namespace/example-project.git",
+			"git_http_url": "http://example.com/exm-namespace/example-project.git",
+			"namespace": "exm-namespace",
+			"visibility_level": 0,
+			"path_with_namespace": "exm-namespace/example-project",
+			"default_branch": "master",
+			"homepage": "http://example.com/exm-namespace/example-project",
+			"url": "git@example.com:exm-namespace/example-project.git",
+			"ssh_url": "git@example.com:exm-namespace/example-project.git",
+			"http_url": "http://example.com/exm-namespace/example-project.git"
+		},
+		"target": {
+			"name": "example-project",
+			"description": "",
+			"web_url": "http://example.com/exm-namespace/example-project",
+			"avatar_url": null,
+			"git_ssh_url": "git@example.com:exm-namespace/example-project.git",
+			"git_http_url": "http://example.com/exm-namespace/example-project.git",
+			"namespace": "exm-namespace",
+			"visibility_level": 0,
+			"path_with_namespace": "exm-namespace/example-project",
+			"default_branch": "master",
+			"homepage": "http://example.com/exm-namespace/example-project",
+			"url": "git@example.com:exm-namespace/example-project.git",
+			"ssh_url": "git@example.com:exm-namespace/example-project.git",
+			"http_url": "http://example.com/exm-namespace/example-project.git"
+		},
+		"last_commit": {
+			"id": "61b6a0d35dbaf915760233b637622e383d3cc9ec",
+			"message": "commit message",
+			"timestamp": "2016-12-01T15:07:53+02:00",
+			"url": "http://example.com/exm-namespace/example-project/commit/61b6a0d35dbaf915760233b637622e383d3cc9ec",
+			"author": {
+				"name": "Test User",
+				"email": "test.user@mail.com"
+			}
+		},
+		"work_in_progress": false,
+		"url": "http://example.com/exm-namespace/example-project/merge_requests/1402",
+		"action": "merge"
+	},
+	"repository": {
+		"name": "example-project",
+		"url": "git@example.com:exm-namespace/example-project.git",
+		"description": "",
+		"homepage": "http://example.com/exm-namespace/example-project"
+	},
+	"assignee": {
+		"name": "Administrator",
+		"username": "root",
+		"avatar_url": "http://www.gravatar.com/avatar/d22738dc40839e3d95fca77ca3eac067?s=80\u0026d=identicon"
+	}
+}`
+
+	var event *MergeEvent
+	err := json.Unmarshal([]byte(jsonObject), &event)
+
+	if err != nil {
+		t.Errorf("Group Merge Event can not unmarshaled: %v\n ", err.Error())
+	}
+
+	if event == nil {
+		t.Errorf("Group Merge Event is null")
+	}
+
+	if event.ObjectKind != "merge_request" {
+		t.Errorf("ObjectKind is %v, want %v", event.ObjectKind, "merge_request")
+	}
+
+	if event.User.Username != "root" {
+		t.Errorf("User.Username is %v, want %v", event.User.Username, "root")
+	}
+
+	if event.Project.Name != "example-project" {
+		t.Errorf("Project.Name is %v, want %v", event.Project.Name, "example-project")
+	}
+
+	if event.ObjectAttributes.ID != 15917 {
+		t.Errorf("ObjectAttributes.ID is %v, want %v", event.ObjectAttributes.ID, 15917)
+	}
+
+	if event.ObjectAttributes.Source.Name != "example-project" {
+		t.Errorf("ObjectAttributes.Source.Name is %v, want %v", event.ObjectAttributes.Source.Name, "example-project")
+	}
+
+	if event.ObjectAttributes.LastCommit.Author.Email != "test.user@mail.com" {
+		t.Errorf("ObjectAttributes.LastCommit.Author.Email is %v, want %v", event.ObjectAttributes.LastCommit.Author.Email, "test.user@mail.com")
+	}
+
+	if event.Repository.Name != "example-project" {
+		t.Errorf("Repository.Name is %v, want %v", event.Repository.Name, "example-project")
+	}
+
+	if event.Assignee.Username != "root" {
+		t.Errorf("Assignee.Username is %v, want %v", event.Assignee, "root")
 	}
 }
