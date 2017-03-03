442ae38dfd2f6a1e94d5f384bb6df1784395e732
Sander van Harmelen <sander@xanzy.io>
2017-03-03 20:43:12 +0100

    Update Commit struct
    Add missing fields

--- commits.go
+++ commits.go
@@ -34,16 +34,18 @@ type CommitsService struct {
 //
 // GitLab API docs: https://docs.gitlab.com/ce/api/commits.html
 type Commit struct {
-	ID            string     `json:"id"`
-	ShortID       string     `json:"short_id"`
-	Title         string     `json:"title"`
-	AuthorName    string     `json:"author_name"`
-	AuthorEmail   string     `json:"author_email"`
-	AuthoredDate  *time.Time `json:"authored_date"`
-	CommittedDate *time.Time `json:"committed_date"`
-	CreatedAt     *time.Time `json:"created_at"`
-	Message       string     `json:"message"`
-	ParentIDs     []string   `json:"parent_ids"`
+	ID             string     `json:"id"`
+	ShortID        string     `json:"short_id"`
+	Title          string     `json:"title"`
+	AuthorName     string     `json:"author_name"`
+	AuthorEmail    string     `json:"author_email"`
+	AuthoredDate   *time.Time `json:"authored_date"`
+	CommitterName  string     `json:"committer_name"`
+	CommitterEmail string     `json:"committer_email"`
+	CommittedDate  *time.Time `json:"committed_date"`
+	CreatedAt      *time.Time `json:"created_at"`
+	Message        string     `json:"message"`
+	ParentIDs      []string   `json:"parent_ids"`
 }
 
 func (c Commit) String() string {
