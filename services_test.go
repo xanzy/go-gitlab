2ff1fb95ce19c8f746ca2aed5246485197bda3b3
Sander van Harmelen <sander@xanzy.io>
2017-10-04 09:36:43 +0200

    Tweak PR #222
    Fixup PR #222 just a little. Most important fix that was needed, was the return type used for the Get call.

--- services_test.go
+++ services_test.go
@@ -7,6 +7,25 @@ import (
 	"testing"
 )
 
+func TestGetDroneCIService(t *testing.T) {
+	mux, server, client := setup()
+	defer teardown(server)
+
+	mux.HandleFunc("/projects/1/services/drone-ci", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"id":1}`)
+	})
+	want := &DroneCIService{Service: Service{ID: 1}}
+
+	service, _, err := client.Services.GetDroneCIService(1)
+	if err != nil {
+		t.Fatalf("Services.GetDroneCIService returns an error: %v", err)
+	}
+	if !reflect.DeepEqual(want, service) {
+		t.Errorf("Services.GetDroneCIService returned %+v, want %+v", service, want)
+	}
+}
+
 func TestSetDroneCIService(t *testing.T) {
 	mux, server, client := setup()
 	defer teardown(server)
@@ -16,8 +35,8 @@ func TestSetDroneCIService(t *testing.T) {
 	})
 
 	opt := &SetDroneCIServiceOptions{String("t"), String("u"), Bool(true)}
-	_, err := client.Services.SetDroneCIService(1, opt)
 
+	_, err := client.Services.SetDroneCIService(1, opt)
 	if err != nil {
 		t.Fatalf("Services.SetDroneCIService returns an error: %v", err)
 	}
@@ -32,30 +51,27 @@ func TestDeleteDroneCIService(t *testing.T) {
 	})
 
 	_, err := client.Services.DeleteDroneCIService(1)
-
 	if err != nil {
 		t.Fatalf("Services.DeleteDroneCIService returns an error: %v", err)
 	}
 }
 
-func TestGetDroneCIService(t *testing.T) {
+func TestGetSlackService(t *testing.T) {
 	mux, server, client := setup()
 	defer teardown(server)
 
-	mux.HandleFunc("/projects/1/services/drone-ci", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/projects/1/services/slack", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
 		fmt.Fprint(w, `{"id":1}`)
 	})
-	want := &DroneCIService{Service: Service{ID: Int(1)}}
+	want := &SlackService{Service: Service{ID: 1}}
 
-	service, _, err := client.Services.GetDroneCIService(1)
-
+	service, _, err := client.Services.GetSlackService(1)
 	if err != nil {
-		t.Fatalf("Services.GetDroneCIService returns an error: %v", err)
+		t.Fatalf("Services.GetSlackService returns an error: %v", err)
 	}
-
 	if !reflect.DeepEqual(want, service) {
-		t.Errorf("Services.GetDroneCIService returned %+v, want %+v", service, want)
+		t.Errorf("Services.GetSlackService returned %+v, want %+v", service, want)
 	}
 }
 
@@ -72,8 +88,8 @@ func TestSetSlackService(t *testing.T) {
 		Username: String("username"),
 		Channel:  String("#development"),
 	}
-	_, err := client.Services.SetSlackService(1, opt)
 
+	_, err := client.Services.SetSlackService(1, opt)
 	if err != nil {
 		t.Fatalf("Services.SetSlackService returns an error: %v", err)
 	}
@@ -88,29 +104,7 @@ func TestDeleteSlackService(t *testing.T) {
 	})
 
 	_, err := client.Services.DeleteSlackService(1)
-
 	if err != nil {
 		t.Fatalf("Services.DeleteSlackService returns an error: %v", err)
-	}
-}
-
-func TestGetSlackService(t *testing.T) {
-	mux, server, client := setup()
-	defer teardown(server)
-
-	mux.HandleFunc("/projects/1/services/slack", func(w http.ResponseWriter, r *http.Request) {
-		testMethod(t, r, "GET")
-		fmt.Fprint(w, `{"id":1}`)
-	})
-	want := &SetSlackServiceOptions{}
-
-	service, _, err := client.Services.GetSlackService(1)
-
-	if err != nil {
-		t.Fatalf("Services.GetSlackService returns an error: %v", err)
-	}
-
-	if !reflect.DeepEqual(want, service) {
-		t.Errorf("Services.GetSlackService returned %+v, want %+v", service, want)
 	}
 }
