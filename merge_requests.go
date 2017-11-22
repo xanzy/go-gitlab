6fe2eff0b5349bc7faec390fd3512529f2a6eb6e
antony360 <huret.antoine@yahoo.fr>
2017-11-22 16:58:57 +0100

    replaced milestone representing struct into merge_requests.go (#5)

--- merge_requests.go
+++ merge_requests.go
@@ -60,33 +60,23 @@ type MergeRequest struct {
 		State     string     `json:"state"`
 		CreatedAt *time.Time `json:"created_at"`
 	} `json:"assignee"`
-	SourceProjectID int      `json:"source_project_id"`
-	TargetProjectID int      `json:"target_project_id"`
-	Labels          []string `json:"labels"`
-	Description     string   `json:"description"`
-	WorkInProgress  bool     `json:"work_in_progress"`
-	Milestone       struct {
-		ID          int        `json:"id"`
-		IID         int        `json:"iid"`
-		ProjectID   int        `json:"project_id"`
-		Title       string     `json:"title"`
-		Description string     `json:"description"`
-		State       string     `json:"state"`
-		CreatedAt   *time.Time `json:"created_at"`
-		UpdatedAt   *time.Time `json:"updated_at"`
-		DueDate     string     `json:"due_date"`
-	} `json:"milestone"`
-	MergeWhenPipelineSucceeds bool   `json:"merge_when_pipeline_succeeds"`
-	MergeStatus               string `json:"merge_status"`
-	Subscribed                bool   `json:"subscribed"`
-	SHA                       string `json:"sha"`
-	MergeCommitShaSHA         string `json:"merge_commit_sha"`
-	UserNotesCount            int    `json:"user_notes_count"`
-	ChangesCount              int    `json:"changes_count"`
-	SouldRemoveSourceBranch   bool   `json:"should_remove_source_branch"`
-	ForceRemoveSourceBranch   bool   `json:"force_remove_source_branch"`
-	WebURL                    string `json:"web_url"`
-	DiscussionLocked          bool   `json:"discussion_locked"`
+	SourceProjectID           int        `json:"source_project_id"`
+	TargetProjectID           int        `json:"target_project_id"`
+	Labels                    []string   `json:"labels"`
+	Description               string     `json:"description"`
+	WorkInProgress            bool       `json:"work_in_progress"`
+	Milestone                 *Milestone `json:"milestone"`
+	MergeWhenPipelineSucceeds bool       `json:"merge_when_pipeline_succeeds"`
+	MergeStatus               string     `json:"merge_status"`
+	Subscribed                bool       `json:"subscribed"`
+	SHA                       string     `json:"sha"`
+	MergeCommitShaSHA         string     `json:"merge_commit_sha"`
+	UserNotesCount            int        `json:"user_notes_count"`
+	ChangesCount              int        `json:"changes_count"`
+	SouldRemoveSourceBranch   bool       `json:"should_remove_source_branch"`
+	ForceRemoveSourceBranch   bool       `json:"force_remove_source_branch"`
+	WebURL                    string     `json:"web_url"`
+	DiscussionLocked          bool       `json:"discussion_locked"`
 	Changes                   []struct {
 		OldPath     string `json:"old_path"`
 		NewPath     string `json:"new_path"`
