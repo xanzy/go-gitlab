b1cfdff1e1e0a9f6c71cd0955d17cd6ba541e053
FlakM <maciej.jan.flak@gmail.com>
2018-02-08 10:12:08 +0100

    Runners api complete implementation (#316)
    * impl DisableProjectRunner
    
    * impl ListRunnersJobs
    
    * impl RemoveARunner
    
    * impl UpdateRunnersDetails
    
    * requested changes
    
    * renamed ListRunnersJobs to ListRunnerJobs

--- runners.go
+++ runners.go
@@ -19,6 +19,7 @@ package gitlab
 import (
 	"fmt"
 	"net/url"
+	"time"
 )
 
 // RunnersService handles communication with the runner related methods of the
@@ -40,6 +41,34 @@ type Runner struct {
 	Name        string `json:"name"`
 	Online      bool   `json:"online"`
 	Status      string `json:"status"`
+}
+
+// RunnersDetails represents a GitLab CI RunnerDetails.
+//
+// GitLab API docs: https://docs.gitlab.com/ce/api/runners.html
+type RunnersDetails struct {
+	Active       bool       `json:"active"`
+	Architecture string     `json:"architecture"`
+	Description  string     `json:"description"`
+	ID           int        `json:"id"`
+	IsShared     bool       `json:"is_shared"`
+	ContactedAt  *time.Time `json:"contacted_at,omitempty"`
+	Name         string     `json:"name"`
+	Online       bool       `json:"online"`
+	Status       string     `json:"status"`
+	Platform     string     `json:"platform,omitempty"`
+	Projects     []struct {
+		ID                int    `json:"id"`
+		Name              string `json:"name"`
+		NameWithNamespace string `json:"name_with_namespace"`
+		Path              string `json:"path"`
+		PathWithNamespace string `json:"path_with_namespace"`
+	} `json:"projects"`
+	Token       string   `json:"Token"`
+	Revision    string   `json:"revision,omitempty"`
+	TagList     []string `json:"tag_list"`
+	Version     string   `json:"version,omitempty"`
+	AccessLevel string   `json:"access_level"`
 }
 
 // ListRunnersOptions represents the available ListRunners() options.
@@ -82,6 +111,123 @@ func (s *RunnersService) ListAllRunners(opt *ListRunnersOptions, options ...Opti
 	}
 
 	var rs []*Runner
+	resp, err := s.client.Do(req, &rs)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return rs, resp, err
+}
+
+// GetRunnerDetails returns details for given runner.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/runners.html#get-runner-39-s-details
+func (s *RunnersService) GetRunnerDetails(rid interface{}, options ...OptionFunc) (*RunnersDetails, *Response, error) {
+	runner, err := parseID(rid)
+	if err != nil {
+		return nil, nil, err
+	}
+	u := fmt.Sprintf("runners/%s", runner)
+
+	req, err := s.client.NewRequest("GET", u, nil, options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var rs *RunnersDetails
+	resp, err := s.client.Do(req, &rs)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return rs, resp, err
+}
+
+// UpdateRunnersDetailsOptions represents the available UpdateRunnersDetails() options.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/runners.html#update-runner-39-s-details
+type UpdateRunnersDetailsOptions struct {
+	Description *string  `url:"description,omitempty" json:"description,omitempty"`
+	Active      *bool    `url:"active,omitempty" json:"active,omitempty"`
+	TagList     []string `url:"tag_list[],omitempty" json:"tag_list,omitempty"`
+	RunUntagged *bool    `url:"run_untagged,omitempty" json:"run_untagged,omitempty"`
+	Locked      *bool    `url:"locked,omitempty" json:"locked,omitempty"`
+	AccessLevel *string  `url:"access_level,omitempty" json:"access_level,omitempty"`
+}
+
+// UpdateRunnersDetails updates runners details
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/runners.html#update-runner-39-s-details
+func (s *RunnersService) UpdateRunnersDetails(rid interface{}, opt *UpdateRunnersDetailsOptions, options ...OptionFunc) (*RunnersDetails, *Response, error) {
+	runner, err := parseID(rid)
+	if err != nil {
+		return nil, nil, err
+	}
+	u := fmt.Sprintf("runners/%s", runner)
+
+	req, err := s.client.NewRequest("PUT", u, opt, options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var rs *RunnersDetails
+	resp, err := s.client.Do(req, &rs)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return rs, resp, err
+}
+
+// RemoveARunner removes a runner
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/runners.html#remove-a-runner
+func (s *RunnersService) RemoveARunner(rid interface{}, options ...OptionFunc) (*Response, error) {
+	runner, err := parseID(rid)
+	if err != nil {
+		return nil, err
+	}
+	u := fmt.Sprintf("runners/%s", runner)
+
+	req, err := s.client.NewRequest("DELETE", u, nil, options)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(req, nil)
+}
+
+// ListRunnersJobsOptions represents the available ListRunnersJobs()
+// options. (one of running, success, failed, canceled)
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/runners.html#list-runner-39-s-jobs
+type ListRunnersJobsOptions struct {
+	ListOptions
+	Status *BuildState `url:"status,omitempty" json:"status,omitempty"`
+}
+
+// ListRunnerJobs gets a list of jobs that are being processed or were processed by specified Runner.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/runners.html#list-runner-39-s-jobs
+func (s *RunnersService) ListRunnerJobs(rid interface{}, opt *ListRunnersJobsOptions, options ...OptionFunc) ([]*Job, *Response, error) {
+	runner, err := parseID(rid)
+	if err != nil {
+		return nil, nil, err
+	}
+	u := fmt.Sprintf("runners/%s/jobs", runner)
+
+	req, err := s.client.NewRequest("GET", u, opt, options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var rs []*Job
 	resp, err := s.client.Do(req, &rs)
 	if err != nil {
 		return nil, resp, err
@@ -154,4 +300,29 @@ func (s *RunnersService) EnableProjectRunner(pid interface{}, opt *EnableProject
 	}
 
 	return r, resp, err
+}
+
+// DisableProjectRunner disables a specific runner from project.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/runners.html#disable-a-runner-from-project
+func (s *RunnersService) DisableProjectRunner(pid interface{}, rid interface{}, options ...OptionFunc) (*Response, error) {
+	project, err := parseID(pid)
+	if err != nil {
+		return nil, err
+	}
+
+	runner, err := parseID(rid)
+	if err != nil {
+		return nil, err
+	}
+
+	u := fmt.Sprintf("projects/%s/runners/%s", url.QueryEscape(project), url.QueryEscape(runner))
+
+	req, err := s.client.NewRequest("DELETE", u, nil, options)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(req, nil)
 }
