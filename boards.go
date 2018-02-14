93b28cd2f67c3f5a7a420f1f3b21fba6c75d57b7
Aurelien Girard <a.girard@oodrive.com>
2018-02-14 11:11:28 +0100

    Add all Available API on Boards for GitLab

--- boards.go
+++ boards.go
@@ -25,7 +25,7 @@ import (
 // of the GitLab API.
 //
 // GitLab API docs:
-// https://gitlab.com/gitlab-org/gitlab-ce/blob/8-16-stable/doc/api/boards.md
+// https://docs.gitlab.com/ce/api/boards.html
 type BoardsService struct {
 	client *Client
 }
@@ -33,7 +33,7 @@ type BoardsService struct {
 // Board represents a GitLab board.
 //
 // GitLab API docs:
-// https://gitlab.com/gitlab-org/gitlab-ce/blob/8-16-stable/doc/api/boards.md
+// https://docs.gitlab.com/ce/api/boards.html
 type Board struct {
 	ID        int         `json:"id"`
 	Name      string      `json:"name"`
@@ -45,7 +45,7 @@ type Board struct {
 // BoardList represents a GitLab board list item.
 //
 // GitLab API docs:
-// https://gitlab.com/gitlab-org/gitlab-ce/blob/8-16-stable/doc/api/boards.md
+// https://docs.gitlab.com/ce/api/boards.html
 type BoardList struct {
 	ID       int   `json:"id"`
 	Label    Label `json:"label"`
@@ -63,7 +63,7 @@ func (b BoardList) String() string {
 // ListProjectBoards gets a list of all boards of project.
 //
 // GitLab API docs:
-// https://gitlab.com/gitlab-org/gitlab-ce/blob/8-16-stable/doc/api/boards.md#project-board
+// https://docs.gitlab.com/ce/api/boards.html#project-board
 func (b *BoardsService) ListProjectBoards(pid interface{}) ([]*Board, *Response, error) {
 	project, err := parseID(pid)
 	if err != nil {
@@ -85,11 +85,11 @@ func (b *BoardsService) ListProjectBoards(pid interface{}) ([]*Board, *Response,
 	return boards, resp, err
 }
 
-// ListProjectBoard gets a single board list of project.
+// GetProjectBoard gets a single board of project.
 //
 // GitLab API docs:
-// https://gitlab.com/gitlab-org/gitlab-ce/blob/8-16-stable/doc/api/boards.md#project-board
-func (b *BoardsService) ListProjectBoard(pid interface{}, bid interface{}) (*Board, *Response, error) {
+// https://docs.gitlab.com/ce/api/boards.html#single-board
+func (b *BoardsService) GetProjectBoard(pid interface{}, bid interface{}) (*Board, *Response, error) {
 	project, err := parseID(pid)
 	if err != nil {
 		return nil, nil, err
@@ -115,11 +115,41 @@ func (b *BoardsService) ListProjectBoard(pid interface{}, bid interface{}) (*Boa
 	return board, resp, err
 }
 
-// ListProjectBoardList gets a single board list of project.
+// GetProjectBoardLists gets a list of the board's lists. Does not include backlog and closed lists.
 //
 // GitLab API docs:
-// https://gitlab.com/gitlab-org/gitlab-ce/blob/8-16-stable/doc/api/boards.md#project-board
-func (b *BoardsService) ListProjectBoardList(pid interface{}, bid interface{}, lid interface{}) (*BoardList, *Response, error) {
+// https://docs.gitlab.com/ce/api/boards.html#single-board
+func (b *BoardsService) GetProjectBoardLists(pid interface{}, bid interface{}) ([]*BoardList, *Response, error) {
+	project, err := parseID(pid)
+	if err != nil {
+		return nil, nil, err
+	}
+	boardID, err := parseID(bid)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	u := fmt.Sprintf("projects/%s/boards/%s/lists", url.QueryEscape(project), url.QueryEscape(boardID))
+
+	req, err := b.client.NewRequest("GET", u, nil, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var boardLists []*BoardList
+	resp, err := b.client.Do(req, &boardLists)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return boardLists, resp, err
+}
+
+// GetProjectBoardList gets a single board list of project.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/boards.html#project-board
+func (b *BoardsService) GetProjectBoardList(pid interface{}, bid interface{}, lid interface{}) (*BoardList, *Response, error) {
 	project, err := parseID(pid)
 	if err != nil {
 		return nil, nil, err
@@ -147,4 +177,101 @@ func (b *BoardsService) ListProjectBoardList(pid interface{}, bid interface{}, l
 	}
 
 	return boardList, resp, err
+}
+
+// CreateBoardList creates a new Issue Board list.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/boards.html#new-board-list
+func (b *BoardsService) CreateBoardList(pid interface{}, bid interface{}, lid interface{}) (*BoardList, *Response, error) {
+	project, err := parseID(pid)
+	if err != nil {
+		return nil, nil, err
+	}
+	boardID, err := parseID(bid)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	labelID, err := parseID(lid)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	u := fmt.Sprintf("projects/%s/boards/%s/lists?label_id=%s", url.QueryEscape(project), url.QueryEscape(boardID), url.QueryEscape(labelID))
+
+	req, err := b.client.NewRequest("POST", u, nil, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	bl := new(BoardList)
+	resp, err := b.client.Do(req, bl)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return bl, resp, err
+}
+
+// UpdateProjectBoardListPosition updates an existing Issue Board list. This call is used to change list position.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/boards.html#edit-board-list
+func (b *BoardsService) UpdateProjectBoardListPosition(pid interface{}, bid interface{}, lid interface{}, position int) (*BoardList, *Response, error) {
+	project, err := parseID(pid)
+	if err != nil {
+		return nil, nil, err
+	}
+	boardID, err := parseID(bid)
+	if err != nil {
+		return nil, nil, err
+	}
+	listID, err := parseID(lid)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	u := fmt.Sprintf("projects/%s/boards/%s/lists/%s?position=%d", url.QueryEscape(project), url.QueryEscape(boardID), url.QueryEscape(listID), position)
+
+	req, err := b.client.NewRequest("PUT", u, nil, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var boardList *BoardList
+	resp, err := b.client.Do(req, &boardList)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return boardList, resp, err
+}
+
+// DeleteProjectBoardListPosition Only for admins and project owners. Soft deletes the board list in question.
+//
+// GitLab API docs:
+// https://docs.gitlab.com/ce/api/boards.html#delete-a-board-list
+func (b *BoardsService) DeleteProjectBoardListPosition(pid interface{}, bid interface{}, lid interface{}) (*Response, error) {
+	project, err := parseID(pid)
+	if err != nil {
+		return nil, err
+	}
+	boardID, err := parseID(bid)
+	if err != nil {
+		return nil, err
+	}
+	listID, err := parseID(lid)
+	if err != nil {
+		return nil, err
+	}
+
+	u := fmt.Sprintf("projects/%s/boards/%s/lists/%s", url.QueryEscape(project), url.QueryEscape(boardID), url.QueryEscape(listID))
+
+	req, err := b.client.NewRequest("DELETE", u, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.client.Do(req, nil)
 }
