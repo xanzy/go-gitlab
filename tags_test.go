c00cef4b483bb262426d9b37bd252436dbccac55
starblind <453473+starblind@users.noreply.github.com>
2018-04-06 08:10:38 -0400

    add support for creating releases in tags api (#373)
    * add support for creating releases in tags api
    
    * add support for updating release notes

--- tags_test.go
+++ tags_test.go
@@ -28,3 +28,47 @@ func TestListTags(t *testing.T) {
 		t.Errorf("Tags.ListTags returned %+v, want %+v", tags, want)
 	}
 }
+
+func TestCreateRelease(t *testing.T) {
+	mux, server, client := setup()
+	defer teardown(server)
+
+	mux.HandleFunc("/projects/1/repository/tags/1.0.0/release", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		fmt.Fprint(w, `{"tag_name": "1.0.0", "description": "Amazing release. Wow"}`)
+	})
+
+	opt := &CreateReleaseOptions{Description: String("Amazing release. Wow")}
+
+	release, _, err := client.Tags.CreateRelease(1, "1.0.0", opt)
+	if err != nil {
+		t.Errorf("Tags.CreateRelease returned error: %v", err)
+	}
+
+	want := &Release{TagName: "1.0.0", Description: "Amazing release. Wow"}
+	if !reflect.DeepEqual(want, release) {
+		t.Errorf("Tags.CreateRelease returned %+v, want %+v", release, want)
+	}
+}
+
+func TestUpdateRelease(t *testing.T) {
+	mux, server, client := setup()
+	defer teardown(server)
+
+	mux.HandleFunc("/projects/1/repository/tags/1.0.0/release", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PUT")
+		fmt.Fprint(w, `{"tag_name": "1.0.0", "description": "Amazing release. Wow!"}`)
+	})
+
+	opt := &UpdateReleaseOptions{Description: String("Amazing release. Wow!")}
+
+	release, _, err := client.Tags.UpdateRelease(1, "1.0.0", opt)
+	if err != nil {
+		t.Errorf("Tags.UpdateRelease returned error: %v", err)
+	}
+
+	want := &Release{TagName: "1.0.0", Description: "Amazing release. Wow!"}
+	if !reflect.DeepEqual(want, release) {
+		t.Errorf("Tags.UpdateRelease returned %+v, want %+v", release, want)
+	}
+}
