6e5187b84a0a2f77fa5834db18428fd6f28a05dc
Minh-Quan TRAN <account@itscaro.me>
2019-03-22 14:37:05 +0100

    text_label & is_project_label are in master branch, current release is 11.8
    Signed-off-by: Minh-Quan TRAN <account@itscaro.me>

--- labels.go
+++ labels.go
@@ -37,12 +37,14 @@ type Label struct {
 	ID                     int    `json:"id"`
 	Name                   string `json:"name"`
 	Color                  string `json:"color"`
+	TextColor              string `json:"text_color"` //11.9+
 	Description            string `json:"description"`
 	OpenIssuesCount        int    `json:"open_issues_count"`
 	ClosedIssuesCount      int    `json:"closed_issues_count"`
 	OpenMergeRequestsCount int    `json:"open_merge_requests_count"`
 	Subscribed             bool   `json:"subscribed"`
 	Priority               int    `json:"priority"`
+	IsProjectLabel         bool   `json:"is_project_label"` //11.9+
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface.
