b3b5892a14c397ada60cbde5fa7a93c7914980b4
Daniel Mican <dm03514@gmail.com>
2020-01-03 19:07:04 -0500

    JobEvent: Adds pipeline_id

--- event_types.go
+++ event_types.go
@@ -203,6 +203,7 @@ type JobEvent struct {
 	BuildFinishedAt   string  `json:"build_finished_at"`
 	BuildDuration     float64 `json:"build_duration"`
 	BuildAllowFailure bool    `json:"build_allow_failure"`
+	PipelineID        int     `json:"pipeline_id"`
 	ProjectID         int     `json:"project_id"`
 	ProjectName       string  `json:"project_name"`
 	User              struct {
