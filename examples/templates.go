0e0c4880f83408a1e691429ee893b37b25b464e4
prytoegrian <prytoegrian@pm.me>
2020-03-31 18:40:05 +0200

    moved ci and gitignore templates

--- examples/templates.go
+++ examples/templates.go
@@ -11,11 +11,23 @@ func templateExample() {
 	git.SetBaseURL("http://localhost:80/api/v4")
 
 	// List all ci templates
-	t, _, err := git.CIYMLTemplate.ListAllTemplates(&gitlab.ListCIYMLTemplatesOptions{})
+	c := template.NewCITemplate(git)
+	t, _, err := c.ListAllTemplates(&template.ListCIYMLTemplatesOptions{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Found %d templates :", len(t))
+	log.Printf("Found %d CI templates :", len(t))
+	for _, template := range t {
+		log.Printf("Found template : %v", template)
+	}
+
+	// List all gitignore templates
+	gitignore := template.NewGitignoreTemplate(git)
+	g, _, err := gitignore.ListTemplates(&template.ListTemplatesOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Found %d Gitignore templates :", len(g))
 	for _, template := range t {
 		log.Printf("Found template : %v", template)
 	}
