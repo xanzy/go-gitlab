5e7147455cb04917609e776bd578e62efc69fc9e
David Busey <davidmbusey@gmail.com>
2020-08-07 11:05:49 -0400

    Update epics.go
    Added more parameters for ListGroupEpicsOptions

--- epics.go
+++ epics.go
@@ -59,12 +59,23 @@ func (e Epic) String() string {
 // GitLab API docs: https://docs.gitlab.com/ee/api/epics.html#list-epics-for-a-group
 type ListGroupEpicsOptions struct {
 	ListOptions
-	State    *string `url:"state,omitempty" json:"state,omitempty"`
-	Labels   Labels  `url:"labels,comma,omitempty" json:"labels,omitempty"`
-	AuthorID *int    `url:"author_id,omitempty" json:"author_id,omitempty"`
-	OrderBy  *string `url:"order_by,omitempty" json:"order_by,omitempty"`
-	Sort     *string `url:"sort,omitempty" json:"sort,omitempty"`
-	Search   *string `url:"search,omitempty" json:"search,omitempty"`
+	State            *string    `url:"state,omitempty" json:"state,omitempty"`
+	Labels           Labels     `url:"labels,comma,omitempty" json:"labels,omitempty"`
+	WithLabelDetails *bool      `url:"with_labels_details,omitempty" json:"with_labels_details,omitempty"`
+	Milestone        *string    `url:"milestone,omitempty" json:"milestone,omitempty"`
+	Scope            *string    `url:"scope,omitempty" json:"scope,omitempty"`
+	AuthorID         *int       `url:"author_id,omitempty" json:"author_id,omitempty"`
+	AssigneeID       *int       `url:"assignee_id,omitempty" json:"assignee_id,omitempty"`
+	MyReactionEmoji  *string    `url:"my_reaction_emoji,omitempty" json:"my_reaction_emoji,omitempty"`
+	IIDs             []int      `url:"iids[],omitempty" json:"iids,omitempty"`
+	OrderBy          *string    `url:"order_by,omitempty" json:"order_by,omitempty"`
+	Sort             *string    `url:"sort,omitempty" json:"sort,omitempty"`
+	Search           *string    `url:"search,omitempty" json:"search,omitempty"`
+	CreatedAfter     *time.Time `url:"created_after,omitempty" json:"created_after,omitempty"`
+	CreatedBefore    *time.Time `url:"created_before,omitempty" json:"created_before,omitempty"`
+	UpdatedAfter     *time.Time `url:"updated_after,omitempty" json:"updated_after,omitempty"`
+	UpdatedBefore    *time.Time `url:"updated_before,omitempty" json:"updated_before,omitempty"`
+	Confidential     *bool      `url:"confidential,omitempty" json:"confidential,omitempty"`
 }
 
 // ListGroupEpics gets a list of group epics. This function accepts pagination
