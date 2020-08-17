e208c8c186fd996a400198ae88f04453b8d6308b
David Busey <davidmbusey@gmail.com>
2020-08-17 13:18:22 -0400

    Removed Duplicate
    Fixed a duplicate.

--- epics.go
+++ epics.go
@@ -226,28 +226,6 @@ func (s *EpicsService) UpdateEpic(gid interface{}, epic int, opt *UpdateEpicOpti
 	return e, resp, err
 }
 
-func (s *EpicsService) GetEpicLinks(gid interface{}, epic int, options ...RequestOptionFunc) ([]*Epic, *Response, error) {
-	group, err := parseID(gid)
-	if err != nil {
-		return nil, nil, err
-	}
-	u := fmt.Sprintf("groups/%s/epics/%d/epics", pathEscape(group), epic)
-
-	req, err := s.client.NewRequest("GET", u, nil, options)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	var e []*Epic
-	resp, err := s.client.Do(req, &e)
-	if err != nil {
-		return nil, resp, err
-	}
-
-	return e, resp, err
-}
-
-
 // DeleteEpic deletes a single group epic.
 //
 // GitLab API docs: https://docs.gitlab.com/ee/api/epics.html#delete-epic
