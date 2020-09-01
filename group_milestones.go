7083d2247ad1806147f263579fd5dadf3fa7d02f
Erik Kristensen <erik@erikkristensen.com>
2020-09-01 17:26:46 -0600

    Update group_milestones.go

--- group_milestones.go
+++ group_milestones.go
@@ -56,9 +56,10 @@ func (m GroupMilestone) String() string {
 // https://docs.gitlab.com/ce/api/group_milestones.html#list-group-milestones
 type ListGroupMilestonesOptions struct {
 	ListOptions
-	IIDs   []int  `url:"iids,omitempty" json:"iids,omitempty"`
-	State  string `url:"state,omitempty" json:"state,omitempty"`
-	Search string `url:"search,omitempty" json:"search,omitempty"`
+	IIDs                    []int  `url:"iids,omitempty" json:"iids,omitempty"`
+	State                   string `url:"state,omitempty" json:"state,omitempty"`
+	Search                  string `url:"search,omitempty" json:"search,omitempty"`
+	IncludeParentMilestones bool   `url:"include_parent_milestones,omitempty" json:"include_parent_milestones,omitempty"
 }
 
 // ListGroupMilestones returns a list of group milestones.
