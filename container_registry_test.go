393cff060cc2e6fa9a4ff3a4e6707dc7f3713ad8
Urento <jan@burzinski.de>
2021-08-29 00:00:46 +0200

    Added Test for DeleteRegistryRepository

--- container_registry_test.go
+++ container_registry_test.go
@@ -61,22 +61,22 @@ func TestListRegistryRepositories(t *testing.T) {
 
 	created_at1, err := time.Parse(timeLayout, "2019-01-10T13:38:57.391Z")
 	if err != nil {
-		t.Errorf("error while parsing time: %v", err)
+		t.Errorf("ContainerRepository.ListRegistryRepositories error while parsing time: %v", err)
 	}
 
 	created_at2, err := time.Parse(timeLayout, "2019-01-10T13:39:08.229Z")
 	if err != nil {
-		t.Errorf("error while parsing time: %v", err)
+		t.Errorf("ContainerRepository.ListRegistryRepositories error while parsing time: %v", err)
 	}
 
 	cleanup_policy_started_at1, err := time.Parse(timeLayout, "2020-01-10T15:40:57.391Z")
 	if err != nil {
-		t.Errorf("error while parsing time: %v", err)
+		t.Errorf("ContainerRepository.ListRegistryRepositories error while parsing time: %v", err)
 	}
 
 	cleanup_policy_started_at2, err := time.Parse(timeLayout, "2020-08-17T03:12:35.489Z")
 	if err != nil {
-		t.Errorf("error while parsing time: %v", err)
+		t.Errorf("ContainerRepository.ListRegistryRepositories error while parsing time: %v", err)
 	}
 
 	want := []*RegistryRepository{
@@ -85,5 +85,19 @@ func TestListRegistryRepositories(t *testing.T) {
 	}
 	if !reflect.DeepEqual(want, repositories) {
 		t.Errorf("ContainerRepository.ListRegistryRepositories returned %+v, want %+v", repositories, want)
+	}
+}
+
+func TestDeleteRegistryRepository(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/5/registry/repositories/2", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+	})
+
+	_, err := client.ContainerRegistry.DeleteRegistryRepository(5, 2)
+	if err != nil {
+		t.Errorf("ContainerRegistry.DeleteRegistryRepository returned error: %v", err)
 	}
 }
