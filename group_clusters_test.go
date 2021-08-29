e47017ac67206e94d4ef7fb7ad378312102bc1c4
Urento <jan@burzinski.de>
2021-08-29 13:33:54 +0200

    Added Test for GetGroupCluster

--- group_clusters_test.go
+++ group_clusters_test.go
@@ -135,3 +135,114 @@ func TestGroupListClusters(t *testing.T) {
 		t.Errorf("GroupCluster.ListClusters returned %+v, want %+v", clusters, want)
 	}
 }
+
+func TestGetGroupCluster(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/groups/26/clusters/18", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		fmt.Fprintf(w, `{
+			"id":18,
+			"name":"cluster-1",
+			"domain":"example.com",
+			"created_at":"2019-01-02T20:18:12.563Z",
+			"managed": true,
+			"enabled": true,
+			"provider_type":"user",
+			"platform_type":"kubernetes",
+			"environment_scope":"*",
+			"cluster_type":"group_type",
+			"user":
+			{
+			  "id":1,
+			  "name":"Administrator",
+			  "username":"root",
+			  "state":"active",
+			  "avatar_url":"https://www.gravatar.com/avatar/4249f4df72b..",
+			  "web_url":"https://gitlab.example.com/root"
+			},
+			"platform_kubernetes":
+			{
+			  "api_url":"https://104.197.68.152",
+			  "authorization_type":"rbac",
+			  "ca_cert":"-----BEGIN CERTIFICATE-----\r\nhFiK1L61owwDQYJKoZIhvcNAQELBQAw\r\nLzEtMCsGA1UEAxMkZDA1YzQ1YjctNzdiMS00NDY0LThjNmEtMTQ0ZDJkZjM4ZDBj\r\nMB4XDTE4MTIyNzIwMDM1MVoXDTIzMTIyNjIxMDM1MVowLzEtMCsGA1UEAxMkZDA1\r\nYzQ1YjctNzdiMS00NDY0LThjNmEtMTQ0ZDJkZjM.......-----END CERTIFICATE-----"
+			},
+			"management_project":
+			{
+			  "id":2,
+			  "description":null,
+			  "name":"project2",
+			  "name_with_namespace":"John Doe8 / project2",
+			  "path":"project2",
+			  "path_with_namespace":"namespace2/project2",
+			  "created_at":"2019-10-11T02:55:54.138Z"
+			},
+			"group":
+			{
+			  "id":26,
+			  "name":"group-with-clusters-api",
+			  "web_url":"https://gitlab.example.com/group-with-clusters-api"
+			}
+		  }`)
+	})
+
+	cluster, _, err := client.GroupCluster.GetCluster(26, 18)
+	if err != nil {
+		t.Errorf("GroupCluster.GetCluster returned error: %v", err)
+	}
+
+	timeLayout := "2006-01-02T15:04:05Z07:00"
+	createdAt, err := time.Parse(timeLayout, "2019-01-02T20:18:12.563Z")
+	if err != nil {
+		t.Errorf("DeployKeys.ListAllDeployKeys returned an error while parsing time: %v", err)
+	}
+
+	createdAt2, err := time.Parse(timeLayout, "2019-10-11T02:55:54.138Z")
+	if err != nil {
+		t.Errorf("DeployKeys.ListAllDeployKeys returned an error while parsing time: %v", err)
+	}
+
+	want := &GroupCluster{
+		ID:               18,
+		Name:             "cluster-1",
+		Domain:           "example.com",
+		CreatedAt:        &createdAt,
+		Managed:          true,
+		Enabled:          true,
+		ProviderType:     "user",
+		PlatformType:     "kubernetes",
+		EnvironmentScope: "*",
+		ClusterType:      "group_type",
+		User: &User{
+			ID:        1,
+			Name:      "Administrator",
+			Username:  "root",
+			State:     "active",
+			AvatarURL: "https://www.gravatar.com/avatar/4249f4df72b..",
+			WebURL:    "https://gitlab.example.com/root",
+		},
+		PlatformKubernetes: &PlatformKubernetes{
+			APIURL:            "https://104.197.68.152",
+			AuthorizationType: "rbac",
+			CaCert:            "-----BEGIN CERTIFICATE-----\r\nhFiK1L61owwDQYJKoZIhvcNAQELBQAw\r\nLzEtMCsGA1UEAxMkZDA1YzQ1YjctNzdiMS00NDY0LThjNmEtMTQ0ZDJkZjM4ZDBj\r\nMB4XDTE4MTIyNzIwMDM1MVoXDTIzMTIyNjIxMDM1MVowLzEtMCsGA1UEAxMkZDA1\r\nYzQ1YjctNzdiMS00NDY0LThjNmEtMTQ0ZDJkZjM.......-----END CERTIFICATE-----",
+		},
+		ManagementProject: &ManagementProject{
+			ID:                2,
+			Description:       nil,
+			Name:              "project2",
+			NameWithNamespace: "John Doe8 / project2",
+			Path:              "project2",
+			PathWithNamespace: "namespace2/project2",
+			CreatedAt:         &createdAt2,
+		},
+		Group: &Group{
+			ID:     26,
+			Name:   "group-with-clusters-api",
+			WebURL: "https://gitlab.example.com/group-with-clusters-api",
+		},
+	}
+	if !reflect.DeepEqual(want, cluster) {
+		t.Errorf("GroupCluster.GetCluster returned %+v, want %+v", cluster, want)
+	}
+}
