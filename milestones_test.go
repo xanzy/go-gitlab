e65203e59266cdb9b183968ce7eced860decc782
Venkatesh Thalluri <venkateshthalluri123@gmail.com>
2021-09-28 15:55:46 +0530

    add tests for create, update, delete in milestones

--- milestones_test.go
+++ milestones_test.go
@@ -91,23 +91,150 @@ func TestMilestonesService_GetMilestone(t *testing.T) {
 		Expired:     Bool(false),
 	}
 
-	ms, resp, err := client.Milestones.GetMilestone(5, 12, nil)
+	m, resp, err := client.Milestones.GetMilestone(5, 12, nil)
 	require.NoError(t, err)
 	require.NotNil(t, resp)
-	require.Equal(t, want, ms)
+	require.Equal(t, want, m)
 
-	ms, resp, err = client.Milestones.GetMilestone(5.01, 12, nil)
+	m, resp, err = client.Milestones.GetMilestone(5.01, 12, nil)
 	require.EqualError(t, err, "invalid ID type 5.01, the ID must be an int or a string")
 	require.Nil(t, resp)
-	require.Nil(t, ms)
+	require.Nil(t, m)
 
-	ms, resp, err = client.Milestones.GetMilestone(5, 12, nil, errorOption)
+	m, resp, err = client.Milestones.GetMilestone(5, 12, nil, errorOption)
 	require.EqualError(t, err, "RequestOptionFunc returns an error")
 	require.Nil(t, resp)
-	require.Nil(t, ms)
+	require.Nil(t, m)
 
-	ms, resp, err = client.Milestones.GetMilestone(3, 12, nil)
+	m, resp, err = client.Milestones.GetMilestone(3, 12, nil)
 	require.Error(t, err)
-	require.Nil(t, ms)
+	require.Nil(t, m)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestMilestonesService_CreateMilestone(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/5/milestones", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		fmt.Fprintf(w, `
+		  {
+			"id": 12,
+			"iid": 3,
+			"project_id": 16,
+			"title": "10.0",
+			"description": "Version",
+			"state": "active",
+			"expired": false
+		  }
+		`)
+	})
+
+	want := &Milestone{
+		ID:          12,
+		IID:         3,
+		ProjectID:   16,
+		Title:       "10.0",
+		Description: "Version",
+		State:       "active",
+		WebURL:      "",
+		Expired:     Bool(false),
+	}
+
+	m, resp, err := client.Milestones.CreateMilestone(5, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, m)
+
+	m, resp, err = client.Milestones.CreateMilestone(5.01, nil)
+	require.EqualError(t, err, "invalid ID type 5.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+	require.Nil(t, m)
+
+	m, resp, err = client.Milestones.CreateMilestone(5, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, m)
+
+	m, resp, err = client.Milestones.CreateMilestone(3, nil)
+	require.Error(t, err)
+	require.Nil(t, m)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestMilestonesService_UpdateMilestone(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/5/milestones/12", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPut)
+		fmt.Fprintf(w, `
+		  {
+			"id": 12,
+			"iid": 3,
+			"project_id": 16,
+			"title": "10.0",
+			"description": "Version",
+			"state": "active",
+			"expired": false
+		  }
+		`)
+	})
+
+	want := &Milestone{
+		ID:          12,
+		IID:         3,
+		ProjectID:   16,
+		Title:       "10.0",
+		Description: "Version",
+		State:       "active",
+		WebURL:      "",
+		Expired:     Bool(false),
+	}
+
+	m, resp, err := client.Milestones.UpdateMilestone(5, 12, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, m)
+
+	m, resp, err = client.Milestones.UpdateMilestone(5.01, 12, nil)
+	require.EqualError(t, err, "invalid ID type 5.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+	require.Nil(t, m)
+
+	m, resp, err = client.Milestones.UpdateMilestone(5, 12, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, m)
+
+	m, resp, err = client.Milestones.UpdateMilestone(3, 12, nil)
+	require.Error(t, err)
+	require.Nil(t, m)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestMilestonesService_DeleteMilestone(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/5/milestones/12", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+	})
+
+	resp, err := client.Milestones.DeleteMilestone(5, 12, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+
+	resp, err = client.Milestones.DeleteMilestone(5.01, 12, nil)
+	require.EqualError(t, err, "invalid ID type 5.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+
+	resp, err = client.Milestones.DeleteMilestone(5, 12, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+
+	resp, err = client.Milestones.DeleteMilestone(3, 12, nil)
+	require.Error(t, err)
 	require.Equal(t, http.StatusNotFound, resp.StatusCode)
 }
