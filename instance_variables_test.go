d07f9fcdc738d4597ce86989bbc4621cf1849057
Venkatesh Thalluri <venkateshthalluri123@gmail.com>
2021-09-30 09:21:27 +0530

    add tests for instance_variables

---
+++ instance_variables_test.go
@@ -0,0 +1,103 @@
+package gitlab
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestInstanceVariablesService_ListVariables(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/admin/ci/variables", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		fmt.Fprintf(w, `
+			[
+				{
+					"key": "TEST_VARIABLE_1",
+					"variable_type": "env_var",
+					"value": "TEST_1",
+					"protected": false,
+					"masked": false
+				}
+			]
+		`)
+	})
+
+	want := []*InstanceVariable{{
+		Key:          "TEST_VARIABLE_1",
+		Value:        "TEST_1",
+		VariableType: "env_var",
+		Protected:    false,
+		Masked:       false,
+	}}
+
+	ivs, resp, err := client.InstanceVariables.ListVariables(nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, ivs)
+
+	ivs, resp, err = client.InstanceVariables.ListVariables(nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, ivs)
+}
+
+func TestInstanceVariablesService_ListVariables_StatusNotFound(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/admin/ci/variables", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		w.WriteHeader(http.StatusNotFound)
+	})
+
+	ivs, resp, err := client.InstanceVariables.ListVariables(nil)
+	require.Error(t, err)
+	require.Nil(t, ivs)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestInstanceVariablesService_GetVariable(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/admin/ci/variables/TEST_VARIABLE_1", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		fmt.Fprintf(w, `
+			{
+				"key": "TEST_VARIABLE_1",
+				"variable_type": "env_var",
+				"value": "TEST_1",
+				"protected": false,
+				"masked": false
+			}
+		`)
+	})
+
+	want := &InstanceVariable{
+		Key:          "TEST_VARIABLE_1",
+		Value:        "TEST_1",
+		VariableType: "env_var",
+		Protected:    false,
+		Masked:       false,
+	}
+
+	iv, resp, err := client.InstanceVariables.GetVariable("TEST_VARIABLE_1", nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, iv)
+
+	iv, resp, err = client.InstanceVariables.GetVariable("TEST_VARIABLE_1", nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, iv)
+
+	iv, resp, err = client.InstanceVariables.GetVariable("TEST_VARIABLE_2", nil)
+	require.Error(t, err)
+	require.Nil(t, iv)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
