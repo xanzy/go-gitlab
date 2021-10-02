2c7bc84b99abe06be1b4ba8ee1b508db174649c4
Venkatesh Thalluri <venkateshthalluri123@gmail.com>
2021-10-02 10:37:39 +0530

    add tests for discussions

---
+++ discussions_test.go
@@ -0,0 +1,326 @@
+package gitlab
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDiscussionsService_ListIssueDiscussions(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/5/issues/11/discussions", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		fmt.Fprintf(w, `
+			[
+			  {
+				"id": "6a9c1750b37d513a43987b574953fceb50b03ce7",
+				"individual_note": false,
+				"notes": [
+				  {
+					"id": 1126,
+					"type": "DiscussionNote",
+					"body": "discussion text",
+					"attachment": null,
+					"author": {
+					  "id": 1,
+					  "name": "Venkatesh Thalluri",
+					  "username": "venky333",
+					  "state": "active",
+					  "avatar_url": "https://www.gravatar.com/avatar/00afb8fb6ab07c3ee3e9c1f38777e2f4?s=80&d=identicon",
+					  "web_url": "http://localhost:3000/venky333"
+					},
+					"system": false,
+					"noteable_id": 3,
+					"noteable_type": "Issue",
+					"noteable_iid": null
+				  }
+				]
+			  }
+			]
+		`)
+	})
+
+	want := []*Discussion{{
+		ID:             "6a9c1750b37d513a43987b574953fceb50b03ce7",
+		IndividualNote: false,
+		Notes: []*Note{{
+			ID:         1126,
+			Type:       "DiscussionNote",
+			Body:       "discussion text",
+			Attachment: "",
+			Title:      "",
+			FileName:   "",
+			Author: struct {
+				ID        int    `json:"id"`
+				Username  string `json:"username"`
+				Email     string `json:"email"`
+				Name      string `json:"name"`
+				State     string `json:"state"`
+				AvatarURL string `json:"avatar_url"`
+				WebURL    string `json:"web_url"`
+			}{
+				ID:        1,
+				Username:  "venky333",
+				Email:     "",
+				Name:      "Venkatesh Thalluri",
+				State:     "active",
+				AvatarURL: "https://www.gravatar.com/avatar/00afb8fb6ab07c3ee3e9c1f38777e2f4?s=80&d=identicon",
+				WebURL:    "http://localhost:3000/venky333",
+			},
+			System:       false,
+			ExpiresAt:    nil,
+			UpdatedAt:    nil,
+			CreatedAt:    nil,
+			NoteableID:   3,
+			NoteableType: "Issue",
+			CommitID:     "",
+			Position:     nil,
+			Resolvable:   false,
+			Resolved:     false,
+			ResolvedBy: struct {
+				ID        int    `json:"id"`
+				Username  string `json:"username"`
+				Email     string `json:"email"`
+				Name      string `json:"name"`
+				State     string `json:"state"`
+				AvatarURL string `json:"avatar_url"`
+				WebURL    string `json:"web_url"`
+			}{},
+			NoteableIID: 0,
+		}},
+	}}
+
+	ds, resp, err := client.Discussions.ListIssueDiscussions(5, 11, nil, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, ds)
+
+	ds, resp, err = client.Discussions.ListIssueDiscussions(5.01, 11, nil, nil)
+	require.EqualError(t, err, "invalid ID type 5.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+	require.Nil(t, ds)
+
+	ds, resp, err = client.Discussions.ListIssueDiscussions(5, 11, nil, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, ds)
+
+	ds, resp, err = client.Discussions.ListIssueDiscussions(3, 11, nil, nil)
+	require.Error(t, err)
+	require.Nil(t, ds)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestDiscussionsService_GetIssueDiscussion(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/5/issues/11/discussions/6a9c1750b37d513a43987b574953fceb50b03ce7", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		fmt.Fprintf(w, `
+		  {
+			"id": "6a9c1750b37d513a43987b574953fceb50b03ce7",
+			"individual_note": false,
+			"notes": [
+			  {
+				"id": 1126,
+				"type": "DiscussionNote",
+				"body": "discussion text",
+				"attachment": null,
+				"author": {
+				  "id": 1,
+				  "name": "Venkatesh Thalluri",
+				  "username": "venky333",
+				  "state": "active",
+				  "avatar_url": "https://www.gravatar.com/avatar/00afb8fb6ab07c3ee3e9c1f38777e2f4?s=80&d=identicon",
+				  "web_url": "http://localhost:3000/venky333"
+				},
+				"system": false,
+				"noteable_id": 3,
+				"noteable_type": "Issue",
+				"noteable_iid": null
+			  }
+			]
+		  }
+		`)
+	})
+
+	want := &Discussion{
+		ID:             "6a9c1750b37d513a43987b574953fceb50b03ce7",
+		IndividualNote: false,
+		Notes: []*Note{{
+			ID:         1126,
+			Type:       "DiscussionNote",
+			Body:       "discussion text",
+			Attachment: "",
+			Title:      "",
+			FileName:   "",
+			Author: struct {
+				ID        int    `json:"id"`
+				Username  string `json:"username"`
+				Email     string `json:"email"`
+				Name      string `json:"name"`
+				State     string `json:"state"`
+				AvatarURL string `json:"avatar_url"`
+				WebURL    string `json:"web_url"`
+			}{
+				ID:        1,
+				Username:  "venky333",
+				Email:     "",
+				Name:      "Venkatesh Thalluri",
+				State:     "active",
+				AvatarURL: "https://www.gravatar.com/avatar/00afb8fb6ab07c3ee3e9c1f38777e2f4?s=80&d=identicon",
+				WebURL:    "http://localhost:3000/venky333",
+			},
+			System:       false,
+			ExpiresAt:    nil,
+			UpdatedAt:    nil,
+			CreatedAt:    nil,
+			NoteableID:   3,
+			NoteableType: "Issue",
+			CommitID:     "",
+			Position:     nil,
+			Resolvable:   false,
+			Resolved:     false,
+			ResolvedBy: struct {
+				ID        int    `json:"id"`
+				Username  string `json:"username"`
+				Email     string `json:"email"`
+				Name      string `json:"name"`
+				State     string `json:"state"`
+				AvatarURL string `json:"avatar_url"`
+				WebURL    string `json:"web_url"`
+			}{},
+			NoteableIID: 0,
+		}},
+	}
+
+	d, resp, err := client.Discussions.GetIssueDiscussion(5, 11, "6a9c1750b37d513a43987b574953fceb50b03ce7", nil, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, d)
+
+	d, resp, err = client.Discussions.GetIssueDiscussion(5.01, 11, "6a9c1750b37d513a43987b574953fceb50b03ce7", nil, nil)
+	require.EqualError(t, err, "invalid ID type 5.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+	require.Nil(t, d)
+
+	d, resp, err = client.Discussions.GetIssueDiscussion(5, 11, "6a9c1750b37d513a43987b574953fceb50b03ce7", nil, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, d)
+
+	d, resp, err = client.Discussions.GetIssueDiscussion(3, 11, "6a9c1750b37d513a43987b574953fceb50b03ce7", nil, nil)
+	require.Error(t, err)
+	require.Nil(t, d)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestDiscussionsService_CreateIssueDiscussion(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/5/issues/11/discussions", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		fmt.Fprintf(w, `
+		  {
+			"id": "6a9c1750b37d513a43987b574953fceb50b03ce7",
+			"individual_note": false,
+			"notes": [
+			  {
+				"id": 1126,
+				"type": "DiscussionNote",
+				"body": "discussion text",
+				"attachment": null,
+				"author": {
+				  "id": 1,
+				  "name": "Venkatesh Thalluri",
+				  "username": "venky333",
+				  "state": "active",
+				  "avatar_url": "https://www.gravatar.com/avatar/00afb8fb6ab07c3ee3e9c1f38777e2f4?s=80&d=identicon",
+				  "web_url": "http://localhost:3000/venky333"
+				},
+				"system": false,
+				"noteable_id": 3,
+				"noteable_type": "Issue",
+				"noteable_iid": null
+			  }
+			]
+		  }
+		`)
+	})
+
+	want := &Discussion{
+		ID:             "6a9c1750b37d513a43987b574953fceb50b03ce7",
+		IndividualNote: false,
+		Notes: []*Note{{
+			ID:         1126,
+			Type:       "DiscussionNote",
+			Body:       "discussion text",
+			Attachment: "",
+			Title:      "",
+			FileName:   "",
+			Author: struct {
+				ID        int    `json:"id"`
+				Username  string `json:"username"`
+				Email     string `json:"email"`
+				Name      string `json:"name"`
+				State     string `json:"state"`
+				AvatarURL string `json:"avatar_url"`
+				WebURL    string `json:"web_url"`
+			}{
+				ID:        1,
+				Username:  "venky333",
+				Email:     "",
+				Name:      "Venkatesh Thalluri",
+				State:     "active",
+				AvatarURL: "https://www.gravatar.com/avatar/00afb8fb6ab07c3ee3e9c1f38777e2f4?s=80&d=identicon",
+				WebURL:    "http://localhost:3000/venky333",
+			},
+			System:       false,
+			ExpiresAt:    nil,
+			UpdatedAt:    nil,
+			CreatedAt:    nil,
+			NoteableID:   3,
+			NoteableType: "Issue",
+			CommitID:     "",
+			Position:     nil,
+			Resolvable:   false,
+			Resolved:     false,
+			ResolvedBy: struct {
+				ID        int    `json:"id"`
+				Username  string `json:"username"`
+				Email     string `json:"email"`
+				Name      string `json:"name"`
+				State     string `json:"state"`
+				AvatarURL string `json:"avatar_url"`
+				WebURL    string `json:"web_url"`
+			}{},
+			NoteableIID: 0,
+		}},
+	}
+
+	d, resp, err := client.Discussions.CreateIssueDiscussion(5, 11, nil, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, d)
+
+	d, resp, err = client.Discussions.CreateIssueDiscussion(5.01, 11, nil, nil)
+	require.EqualError(t, err, "invalid ID type 5.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+	require.Nil(t, d)
+
+	d, resp, err = client.Discussions.CreateIssueDiscussion(5, 11, nil, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, d)
+
+	d, resp, err = client.Discussions.CreateIssueDiscussion(3, 11, nil, nil)
+	require.Error(t, err)
+	require.Nil(t, d)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
