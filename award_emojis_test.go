d695faa01fca5a61f6876326d88279a3e67c14ce
Venkatesh Thalluri <venkateshthalluri123@gmail.com>
2021-10-04 09:16:36 +0530

    add tests for create, delete for merge requests, snippets and issues in award_emojis

--- award_emojis_test.go
+++ award_emojis_test.go
@@ -427,3 +427,285 @@ func TestAwardEmojiService_GetSnippetAwardEmoji(t *testing.T) {
 	require.Nil(t, ae)
 	require.Equal(t, http.StatusNotFound, resp.StatusCode)
 }
+
+func TestAwardEmojiService_CreateMergeRequestAwardEmoji(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/1/merge_requests/80/award_emoji", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		fmt.Fprintf(w, `
+		  {
+			"id": 4,
+			"name": "1234",
+			"user": {
+			  "name": "Venkatesh Thalluri",
+			  "username": "venky333",
+			  "id": 1,
+			  "state": "active",
+			  "avatar_url": "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+			  "web_url": "http://gitlab.example.com/venky333"
+			},
+			"awardable_id": 80,
+			"awardable_type": "Merge request"
+		  }
+		`)
+	})
+
+	want := &AwardEmoji{
+		ID:   4,
+		Name: "1234",
+		User: struct {
+			Name      string `json:"name"`
+			Username  string `json:"username"`
+			ID        int    `json:"id"`
+			State     string `json:"state"`
+			AvatarURL string `json:"avatar_url"`
+			WebURL    string `json:"web_url"`
+		}{
+			Name:      "Venkatesh Thalluri",
+			Username:  "venky333",
+			ID:        1,
+			State:     "active",
+			AvatarURL: "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+			WebURL:    "http://gitlab.example.com/venky333",
+		},
+		CreatedAt:     nil,
+		UpdatedAt:     nil,
+		AwardableID:   80,
+		AwardableType: "Merge request",
+	}
+
+	ae, resp, err := client.AwardEmoji.CreateMergeRequestAwardEmoji(1, 80, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, ae)
+
+	ae, resp, err = client.AwardEmoji.CreateMergeRequestAwardEmoji(1.01, 80, nil)
+	require.EqualError(t, err, "invalid ID type 1.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+	require.Nil(t, ae)
+
+	ae, resp, err = client.AwardEmoji.CreateMergeRequestAwardEmoji(1, 80, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, ae)
+
+	ae, resp, err = client.AwardEmoji.CreateMergeRequestAwardEmoji(3, 80, nil)
+	require.Error(t, err)
+	require.Nil(t, ae)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestAwardEmojiService_CreateIssueAwardEmoji(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/1/issues/80/award_emoji", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		fmt.Fprintf(w, `
+		  {
+			"id": 4,
+			"name": "1234",
+			"user": {
+			  "name": "Venkatesh Thalluri",
+			  "username": "venky333",
+			  "id": 1,
+			  "state": "active",
+			  "avatar_url": "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+			  "web_url": "http://gitlab.example.com/venky333"
+			},
+			"awardable_id": 80,
+			"awardable_type": "Issue"
+		  }
+		`)
+	})
+
+	want := &AwardEmoji{
+		ID:   4,
+		Name: "1234",
+		User: struct {
+			Name      string `json:"name"`
+			Username  string `json:"username"`
+			ID        int    `json:"id"`
+			State     string `json:"state"`
+			AvatarURL string `json:"avatar_url"`
+			WebURL    string `json:"web_url"`
+		}{
+			Name:      "Venkatesh Thalluri",
+			Username:  "venky333",
+			ID:        1,
+			State:     "active",
+			AvatarURL: "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+			WebURL:    "http://gitlab.example.com/venky333",
+		},
+		CreatedAt:     nil,
+		UpdatedAt:     nil,
+		AwardableID:   80,
+		AwardableType: "Issue",
+	}
+
+	ae, resp, err := client.AwardEmoji.CreateIssueAwardEmoji(1, 80, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, ae)
+
+	ae, resp, err = client.AwardEmoji.CreateIssueAwardEmoji(1.01, 80, nil)
+	require.EqualError(t, err, "invalid ID type 1.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+	require.Nil(t, ae)
+
+	ae, resp, err = client.AwardEmoji.CreateIssueAwardEmoji(1, 80, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, ae)
+
+	ae, resp, err = client.AwardEmoji.CreateIssueAwardEmoji(3, 80, nil)
+	require.Error(t, err)
+	require.Nil(t, ae)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestAwardEmojiService_CreateSnippetAwardEmoji(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/1/snippets/80/award_emoji", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		fmt.Fprintf(w, `
+		  {
+			"id": 4,
+			"name": "1234",
+			"user": {
+			  "name": "Venkatesh Thalluri",
+			  "username": "venky333",
+			  "id": 1,
+			  "state": "active",
+			  "avatar_url": "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+			  "web_url": "http://gitlab.example.com/venky333"
+			},
+			"awardable_id": 80,
+			"awardable_type": "Snippet"
+		  }
+		`)
+	})
+
+	want := &AwardEmoji{
+		ID:   4,
+		Name: "1234",
+		User: struct {
+			Name      string `json:"name"`
+			Username  string `json:"username"`
+			ID        int    `json:"id"`
+			State     string `json:"state"`
+			AvatarURL string `json:"avatar_url"`
+			WebURL    string `json:"web_url"`
+		}{
+			Name:      "Venkatesh Thalluri",
+			Username:  "venky333",
+			ID:        1,
+			State:     "active",
+			AvatarURL: "http://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=80&d=identicon",
+			WebURL:    "http://gitlab.example.com/venky333",
+		},
+		CreatedAt:     nil,
+		UpdatedAt:     nil,
+		AwardableID:   80,
+		AwardableType: "Snippet",
+	}
+
+	ae, resp, err := client.AwardEmoji.CreateSnippetAwardEmoji(1, 80, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, ae)
+
+	ae, resp, err = client.AwardEmoji.CreateSnippetAwardEmoji(1.01, 80, nil)
+	require.EqualError(t, err, "invalid ID type 1.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+	require.Nil(t, ae)
+
+	ae, resp, err = client.AwardEmoji.CreateSnippetAwardEmoji(1, 80, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, ae)
+
+	ae, resp, err = client.AwardEmoji.CreateSnippetAwardEmoji(3, 80, nil)
+	require.Error(t, err)
+	require.Nil(t, ae)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestAwardEmojiService_DeleteMergeRequestAwardEmoji(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/1/merge_requests/80/award_emoji/1", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+	})
+
+	resp, err := client.AwardEmoji.DeleteMergeRequestAwardEmoji(1, 80, 1, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+
+	resp, err = client.AwardEmoji.DeleteMergeRequestAwardEmoji(1.01, 80, 1, nil)
+	require.EqualError(t, err, "invalid ID type 1.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+
+	resp, err = client.AwardEmoji.DeleteMergeRequestAwardEmoji(1, 80, 1, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+
+	resp, err = client.AwardEmoji.DeleteMergeRequestAwardEmoji(3, 80, 1, nil)
+	require.Error(t, err)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestAwardEmojiService_DeleteIssueAwardEmoji(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/1/issues/80/award_emoji/1", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+	})
+
+	resp, err := client.AwardEmoji.DeleteIssueAwardEmoji(1, 80, 1, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+
+	resp, err = client.AwardEmoji.DeleteIssueAwardEmoji(1.01, 80, 1, nil)
+	require.EqualError(t, err, "invalid ID type 1.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+
+	resp, err = client.AwardEmoji.DeleteIssueAwardEmoji(1, 80, 1, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+
+	resp, err = client.AwardEmoji.DeleteIssueAwardEmoji(3, 80, 1, nil)
+	require.Error(t, err)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+func TestAwardEmojiService_DeleteSnippetAwardEmoji(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/projects/1/snippets/80/award_emoji/1", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+	})
+
+	resp, err := client.AwardEmoji.DeleteSnippetAwardEmoji(1, 80, 1, nil)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+
+	resp, err = client.AwardEmoji.DeleteSnippetAwardEmoji(1.01, 80, 1, nil)
+	require.EqualError(t, err, "invalid ID type 1.01, the ID must be an int or a string")
+	require.Nil(t, resp)
+
+	resp, err = client.AwardEmoji.DeleteSnippetAwardEmoji(1, 80, 1, nil, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+
+	resp, err = client.AwardEmoji.DeleteSnippetAwardEmoji(3, 80, 1, nil)
+	require.Error(t, err)
+	require.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
