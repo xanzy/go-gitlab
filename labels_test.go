d15c5d4a27be9c7aeccec5f0c9060c5c131f4c2c
Timo Furrer <tuxtimo@gmail.com>
2022-03-07 11:06:55 +0100

    Implement `priority` field in project label creation and update functions

--- labels_test.go
+++ labels_test.go
@@ -30,20 +30,21 @@ func TestCreateLabel(t *testing.T) {
 
 	mux.HandleFunc("/api/v4/projects/1/labels", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodPost)
-		fmt.Fprint(w, `{"id":1, "name": "My Label", "color" : "#11FF22"}`)
+		fmt.Fprint(w, `{"id":1, "name": "My Label", "color" : "#11FF22", "priority": 2}`)
 	})
 
 	// Create new label
 	l := &CreateLabelOptions{
-		Name:  String("My Label"),
-		Color: String("#11FF22"),
+		Name:     String("My Label"),
+		Color:    String("#11FF22"),
+		Priority: Int(2),
 	}
 	label, _, err := client.Labels.CreateLabel("1", l)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	want := &Label{ID: 1, Name: "My Label", Color: "#11FF22"}
+	want := &Label{ID: 1, Name: "My Label", Color: "#11FF22", Priority: 2}
 	if !reflect.DeepEqual(want, label) {
 		t.Errorf("Labels.CreateLabel returned %+v, want %+v", label, want)
 	}
@@ -74,7 +75,7 @@ func TestUpdateLabel(t *testing.T) {
 
 	mux.HandleFunc("/api/v4/projects/1/labels", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodPut)
-		fmt.Fprint(w, `{"id":1, "name": "New Label", "color" : "#11FF23" , "description":"This is updated label"}`)
+		fmt.Fprint(w, `{"id":1, "name": "New Label", "color" : "#11FF23" , "description":"This is updated label", "priority": 42}`)
 	})
 
 	// Update label
@@ -83,6 +84,7 @@ func TestUpdateLabel(t *testing.T) {
 		NewName:     String("New Label"),
 		Color:       String("#11FF23"),
 		Description: String("This is updated label"),
+		Priority:    Int(42),
 	}
 
 	label, resp, err := client.Labels.UpdateLabel("1", l)
@@ -94,7 +96,7 @@ func TestUpdateLabel(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	want := &Label{ID: 1, Name: "New Label", Color: "#11FF23", Description: "This is updated label"}
+	want := &Label{ID: 1, Name: "New Label", Color: "#11FF23", Description: "This is updated label", Priority: 42}
 
 	if !reflect.DeepEqual(want, label) {
 		t.Errorf("Labels.UpdateLabel returned %+v, want %+v", label, want)
