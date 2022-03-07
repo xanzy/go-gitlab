31d8a7473265f8b013cd59eb1fb66e18dea6b8cd
Timo Furrer <tuxtimo@gmail.com>
2022-03-07 10:58:03 +0100

    Complete System Hook API for GitLab 14.9

--- system_hooks_test.go
+++ system_hooks_test.go
@@ -15,13 +15,71 @@ func TestSystemHooksService_ListHooks(t *testing.T) {
 
 	mux.HandleFunc("/api/v4/hooks", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodGet)
-		fmt.Fprint(w, `[{"id":1,"url":"https://gitlab.example.com/hook"}]`)
+		fmt.Fprint(w, `
+		[
+			{
+			  "id":1,
+			  "url":"https://gitlab.example.com/hook",
+			  "created_at":"2016-10-31T12:32:15.192Z",
+			  "push_events":true,
+			  "tag_push_events":false,
+			  "merge_requests_events": true,
+			  "repository_update_events": true,
+			  "enable_ssl_verification":true
+			}
+		]`)
 	})
 
 	hooks, _, err := client.SystemHooks.ListHooks()
 	require.NoError(t, err)
 
-	want := []*Hook{{ID: 1, URL: "https://gitlab.example.com/hook"}}
+	createdAt := time.Date(2016, 10, 31, 12, 32, 15, 192000000, time.UTC)
+	want := []*Hook{{
+		ID:                     1,
+		URL:                    "https://gitlab.example.com/hook",
+		CreatedAt:              &createdAt,
+		PushEvents:             true,
+		TagPushEvents:          false,
+		MergeRequestsEvents:    true,
+		RepositoryUpdateEvents: true,
+		EnableSSLVerification:  true,
+	}}
+	require.Equal(t, want, hooks)
+}
+
+func TestSystemHooksService_GetHook(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v4/hooks/1", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		fmt.Fprint(w, `
+		{
+			"id":1,
+			"url":"https://gitlab.example.com/hook",
+			"created_at":"2016-10-31T12:32:15.192Z",
+			"push_events":true,
+			"tag_push_events":false,
+			"merge_requests_events": true,
+			"repository_update_events": true,
+			"enable_ssl_verification":true
+		}`)
+	})
+
+	hooks, _, err := client.SystemHooks.GetHook(1)
+	require.NoError(t, err)
+
+	createdAt := time.Date(2016, 10, 31, 12, 32, 15, 192000000, time.UTC)
+	want := &Hook{
+		ID:                     1,
+		URL:                    "https://gitlab.example.com/hook",
+		CreatedAt:              &createdAt,
+		PushEvents:             true,
+		TagPushEvents:          false,
+		MergeRequestsEvents:    true,
+		RepositoryUpdateEvents: true,
+		EnableSSLVerification:  true,
+	}
 	require.Equal(t, want, hooks)
 }
 
