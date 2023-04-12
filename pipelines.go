2b2a8762b13c482c886a05785da5542a22337b8b
Pratik Gaikwad <noreplyfrompratik@gmail.com>
2023-04-12 22:45:17 +0000

    fix: `SystemOutput` in the `PipelineTestCase` It is either a null or struct. Not a string.

--- pipelines.go
+++ pipelines.go
@@ -115,10 +115,15 @@ type PipelineTestCases struct {
 	Classname      string         `json:"classname"`
 	File           string         `json:"file"`
 	ExecutionTime  float64        `json:"execution_time"`
-	SystemOutput   string         `json:"system_output"`
+	SystemOutput   SystemOutput   `json:"system_output"`
 	StackTrace     string         `json:"stack_trace"`
 	AttachmentURL  string         `json:"attachment_url"`
 	RecentFailures RecentFailures `json:"recent_failures"`
+}
+
+type SystemOutput struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
 }
 
 // RecentFailures contains failures count for the project's default branch.
