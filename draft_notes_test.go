4f39e920d1d48515715daa7158e24b464f06cb8a
Harrison Cramer <harrisoncramer@gmail.com>
2024-03-30 12:50:59 -0400

    Updated draft_notes_test.go

--- draft_notes_test.go
+++ draft_notes_test.go
@@ -51,3 +51,111 @@ func TestGetDraftNote(t *testing.T) {
 		t.Errorf("DraftNotes.GetDraftNote want %#v, got %#v", note, want)
 	}
 }
+
+func TestListDraftNotes(t *testing.T) {
+	mux, client := setup(t)
+	mux.HandleFunc("/api/v4/projects/1/merge_requests/4329/draft_notes", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		fmt.Fprintf(w, `[
+      {
+        "id": 37349978,
+        "author_id": 10271899,
+        "merge_request_id": 291473309,
+        "resolve_discussion": false,
+        "discussion_id": null,
+        "note": "Some draft note",
+        "commit_id": null,
+        "position": null,
+        "line_code": null
+      },
+      {
+        "id": 37349979,
+        "author_id": 10271899,
+        "merge_request_id": 291473309,
+        "resolve_discussion": false,
+        "discussion_id": null,
+        "note": "Some draft note 2",
+        "commit_id": null,
+        "line_code": "3dacf79e0d779e2baa1c700cf56510e42f55cf85_10_9",
+        "position": {
+          "base_sha": "64581c4ee41beb44d943d7801f82d9038e25e453",
+          "start_sha": "87bffbff93bf334889780f54ae1922355661f867",
+          "head_sha": "2c972dbf9094c380f5f00dcd8112d2c69b24c859",
+          "old_path": "src/some-dir/some-file.js",
+          "new_path": "src/some-dir/some-file.js",
+          "position_type": "text",
+          "old_line": null,
+          "new_line": 9,
+          "line_range": {
+            "start": {
+              "line_code": "3dacf79e0d779e2baa1c700cf56510e42f55cf85_10_9",
+              "type": "new",
+              "old_line": null,
+              "new_line": 9
+            },
+            "end": {
+              "line_code": "3dacf79e0d779e2baa1c700cf56510e42f55cf85_10_9",
+              "type": "new",
+              "old_line": null,
+              "new_line": 9
+            }
+          }
+        }
+      }
+    ]`)
+	})
+
+	notes, _, err := client.DraftNotes.ListDraftNotes("1", 4329, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := []*DraftNote{
+		{
+			ID:                37349978,
+			AuthorID:          10271899,
+			MergeRequestID:    291473309,
+			ResolveDiscussion: false,
+			DiscussionID:      "",
+			Note:              "Some draft note",
+			CommitID:          "",
+			LineCode:          "",
+			Position:          nil,
+		},
+		{
+			ID:                37349979,
+			AuthorID:          10271899,
+			MergeRequestID:    291473309,
+			ResolveDiscussion: false,
+			DiscussionID:      "",
+			Note:              "Some draft note 2",
+			CommitID:          "",
+			LineCode:          "3dacf79e0d779e2baa1c700cf56510e42f55cf85_10_9",
+			Position: &NotePosition{
+				BaseSHA:      "64581c4ee41beb44d943d7801f82d9038e25e453",
+				StartSHA:     "87bffbff93bf334889780f54ae1922355661f867",
+				HeadSHA:      "2c972dbf9094c380f5f00dcd8112d2c69b24c859",
+				OldPath:      "src/some-dir/some-file.js",
+				NewPath:      "src/some-dir/some-file.js",
+				PositionType: "text",
+				NewLine:      9,
+				LineRange: &LineRange{
+					StartRange: &LinePosition{
+						LineCode: "3dacf79e0d779e2baa1c700cf56510e42f55cf85_10_9",
+						Type:     "new",
+						NewLine:  9,
+					},
+					EndRange: &LinePosition{
+						LineCode: "3dacf79e0d779e2baa1c700cf56510e42f55cf85_10_9",
+						Type:     "new",
+						NewLine:  9,
+					},
+				},
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(notes, want) {
+		t.Errorf("DraftNotes.GetDraftNote want %#v, got %#v", notes, want)
+	}
+}
