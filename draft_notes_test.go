394e800c4140f8ca6e90956010e1dce5330a4c12
Harrison Cramer <harrisoncramer@gmail.com>
2024-03-30 13:05:30 -0400

    Moved test data, wrote more tests

--- draft_notes_test.go
+++ draft_notes_test.go
@@ -17,7 +17,6 @@
 package gitlab
 
 import (
-	"fmt"
 	"net/http"
 	"reflect"
 	"testing"
@@ -27,7 +26,7 @@ func TestGetDraftNote(t *testing.T) {
 	mux, client := setup(t)
 	mux.HandleFunc("/api/v4/projects/1/merge_requests/4329/draft_notes/3", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodGet)
-		fmt.Fprint(w, `{"id": 37349978, "author_id": 10271899, "merge_request_id": 291473309, "resolve_discussion": false, "discussion_id": null, "note": "Some draft note", "commit_id": null, "position": null, "line_code": null}`)
+		mustWriteHTTPResponse(t, w, "testdata/get_draft_note.json")
 	})
 
 	note, _, err := client.DraftNotes.GetDraftNote("1", 4329, 3)
@@ -56,53 +55,7 @@ func TestListDraftNotes(t *testing.T) {
 	mux, client := setup(t)
 	mux.HandleFunc("/api/v4/projects/1/merge_requests/4329/draft_notes", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodGet)
-		fmt.Fprintf(w, `[
-      {
-        "id": 37349978,
-        "author_id": 10271899,
-        "merge_request_id": 291473309,
-        "resolve_discussion": false,
-        "discussion_id": null,
-        "note": "Some draft note",
-        "commit_id": null,
-        "position": null,
-        "line_code": null
-      },
-      {
-        "id": 37349979,
-        "author_id": 10271899,
-        "merge_request_id": 291473309,
-        "resolve_discussion": false,
-        "discussion_id": null,
-        "note": "Some draft note 2",
-        "commit_id": null,
-        "line_code": "3dacf79e0d779e2baa1c700cf56510e42f55cf85_10_9",
-        "position": {
-          "base_sha": "64581c4ee41beb44d943d7801f82d9038e25e453",
-          "start_sha": "87bffbff93bf334889780f54ae1922355661f867",
-          "head_sha": "2c972dbf9094c380f5f00dcd8112d2c69b24c859",
-          "old_path": "src/some-dir/some-file.js",
-          "new_path": "src/some-dir/some-file.js",
-          "position_type": "text",
-          "old_line": null,
-          "new_line": 9,
-          "line_range": {
-            "start": {
-              "line_code": "3dacf79e0d779e2baa1c700cf56510e42f55cf85_10_9",
-              "type": "new",
-              "old_line": null,
-              "new_line": 9
-            },
-            "end": {
-              "line_code": "3dacf79e0d779e2baa1c700cf56510e42f55cf85_10_9",
-              "type": "new",
-              "old_line": null,
-              "new_line": 9
-            }
-          }
-        }
-      }
-    ]`)
+		mustWriteHTTPResponse(t, w, "testdata/list_draft_notes.json")
 	})
 
 	notes, _, err := client.DraftNotes.ListDraftNotes("1", 4329, nil)
@@ -164,7 +117,7 @@ func TestCreateDraftNote(t *testing.T) {
 	mux, client := setup(t)
 	mux.HandleFunc("/api/v4/projects/1/merge_requests/4329/draft_notes", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodPost)
-		fmt.Fprint(w, `{"id": 37349980, "author_id": 10271899, "merge_request_id": 291473309, "resolve_discussion": false, "discussion_id": null, "note": "Some new draft note", "commit_id": null, "position": null, "line_code": null}`)
+		mustWriteHTTPResponse(t, w, "testdata/create_draft_note.json")
 	})
 
 	note, _, err := client.DraftNotes.CreateDraftNote("1", 4329, &CreateDraftNoteOptions{
@@ -197,7 +150,7 @@ func TestUpdateDraftNote(t *testing.T) {
 	mux, client := setup(t)
 	mux.HandleFunc("/api/v4/projects/1/merge_requests/4329/draft_notes/3", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodPut)
-		fmt.Fprint(w, `{"id": 37349980, "author_id": 10271899, "merge_request_id": 291473309, "resolve_discussion": false, "discussion_id": null, "note": "Some changed draft note", "commit_id": null, "position": null, "line_code": null}`)
+		mustWriteHTTPResponse(t, w, "testdata/update_draft_note.json")
 	})
 
 	note, _, err := client.DraftNotes.UpdateDraftNote("1", 4329, 3, &UpdateDraftNoteOptions{
@@ -222,5 +175,17 @@ func TestUpdateDraftNote(t *testing.T) {
 
 	if !reflect.DeepEqual(note, want) {
 		t.Errorf("DraftNotes.UpdateDraftNote want %#v, got %#v", note, want)
+	}
+}
+
+func TestDeleteDraftNote(t *testing.T) {
+	mux, client := setup(t)
+	mux.HandleFunc("/api/v4/projects/1/merge_requests/4329/draft_notes/3", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+	})
+
+	_, err := client.DraftNotes.DeleteDraftNote("1", 4329, 3)
+	if err != nil {
+		t.Errorf("DraftNotes.DeleteDraftNote returned error: %v", err)
 	}
 }
