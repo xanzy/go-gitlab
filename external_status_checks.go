a8a47fae6e939e7e6198e6984476fb68db9efb8c
Sander van Harmelen <sander@vanharmelen.nl>
2024-05-05 11:03:31 +0200

    Update function parameters

--- external_status_checks.go
+++ external_status_checks.go
@@ -202,12 +202,12 @@ func (s *ExternalStatusChecksService) UpdateExternalStatusCheck(pid interface{},
 //
 // Gitlab API docs:
 // https://docs.gitlab.com/ee/api/status_checks.html#retry-failed-status-check-for-a-merge-request
-func (s *ExternalStatusChecksService) RetryFailedStatusCheckForAMergeRequest(pid interface{}, mergeRequest int, externalStatusCheckID int, options ...RequestOptionFunc) (*Response, error) {
+func (s *ExternalStatusChecksService) RetryFailedStatusCheckForAMergeRequest(pid interface{}, mergeRequest int, externalStatusCheck int, options ...RequestOptionFunc) (*Response, error) {
 	project, err := parseID(pid)
 	if err != nil {
 		return nil, err
 	}
-	u := fmt.Sprintf("projects/%s/merge_requests/%d/status_checks/%d/retry", PathEscape(project), mergeRequest, externalStatusCheckID)
+	u := fmt.Sprintf("projects/%s/merge_requests/%d/status_checks/%d/retry", PathEscape(project), mergeRequest, externalStatusCheck)
 
 	req, err := s.client.NewRequest(http.MethodPost, u, nil, options)
 	if err != nil {
