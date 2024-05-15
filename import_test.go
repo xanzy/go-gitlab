8e3276d4a9dcf4a6758b8ac654eb0f2a9ba45991
Heidi Berry <heidi.berry@coop.co.uk>
2024-05-15 21:00:52 +0100

    Add more GitHub import API support

--- import_test.go
+++ import_test.go
@@ -3,8 +3,9 @@ package gitlab
 import (
 	"fmt"
 	"net/http"
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestImportService_ImportRepositoryFromGitHub(t *testing.T) {
@@ -47,12 +48,76 @@ func TestImportService_ImportRepositoryFromGitHub(t *testing.T) {
 		TargetNamespace:     Ptr("root"),
 	}
 
-	gi, _, err := client.Import.ImportRepositoryFromGitHub(opt)
-	if err != nil {
-		t.Errorf("Import.ImportRepositoryFromGitHub returned error: %v", err)
+	gi, resp, err := client.Import.ImportRepositoryFromGitHub(opt)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, gi)
+
+	gi, resp, err = client.Import.ImportRepositoryFromGitHub(opt, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, gi)
+}
+
+func TestImportService_CancelGitHubProjectImport(t *testing.T) {
+	mux, client := setup(t)
+
+	mux.HandleFunc("/api/v4/import/github/cancel", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		fmt.Fprintf(w, `
+			{
+				"id": 27,
+				"name": "my-repo",
+				"full_path": "/root/my-repo",
+				"full_name": "Administrator / my-repo",
+				"import_source": "my-github/repo",
+				"import_status": "scheduled",
+				"human_import_status_name": "scheduled",
+				"provider_link": "/my-github/repo"
+			}
+		`)
+	})
+
+	want := &CancelledGitHubImport{
+		ID:                    27,
+		Name:                  "my-repo",
+		FullPath:              "/root/my-repo",
+		FullName:              "Administrator / my-repo",
+		ImportSource:          "my-github/repo",
+		ImportStatus:          "scheduled",
+		HumanImportStatusName: "scheduled",
+		ProviderLink:          "/my-github/repo",
 	}
 
-	if !reflect.DeepEqual(want, gi) {
-		t.Errorf("Import.ImportRepositoryFromGitHub return %+v, want %+v", gi, want)
+	opt := &CancelGitHubProjectImportOptions{
+		ProjectID: Ptr(27),
 	}
+
+	cgi, resp, err := client.Import.CancelGitHubProjectImport(opt)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Equal(t, want, cgi)
+
+	cgi, resp, err = client.Import.CancelGitHubProjectImport(opt, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
+	require.Nil(t, cgi)
+}
+
+func TestImportService_ImportGitHubGistsIntoGitLabSnippets(t *testing.T) {
+	mux, client := setup(t)
+
+	mux.HandleFunc("/api/v4/import/github/gists", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+	})
+
+	opt := &ImportGitHubGistsIntoGitLabSnippetsOptions{PersonalAccessToken: Ptr("token")}
+
+	resp, err := client.Import.ImportGitHubGistsIntoGitLabSnippets(opt)
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+
+	resp, err = client.Import.ImportGitHubGistsIntoGitLabSnippets(opt, errorOption)
+	require.EqualError(t, err, "RequestOptionFunc returns an error")
+	require.Nil(t, resp)
 }
