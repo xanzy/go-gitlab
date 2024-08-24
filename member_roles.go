c0e1eb1ee2601a087fa982d5ca7808ea78374df9
Simon Alexander Alsing <aalsing@gmail.com>
2024-08-24 12:23:00 +0200

    Add properties AdminComplianceFramework, AdminPushRules, AdminWebHook, ManageDeployToken, ManageMergeRequestSettings, ManageSecurityPolicyLink, ReadRunners, RemoveGroup to MemberRoles struct to comply with 17.4.0 spec

--- member_roles.go
+++ member_roles.go
@@ -17,23 +17,31 @@ type MemberRolesService struct {
 //
 // GitLab API docs: https://docs.gitlab.com/ee/api/member_roles.html
 type MemberRole struct {
-	ID                       int              `json:"id"`
-	Name                     string           `json:"name"`
-	Description              string           `json:"description,omitempty"`
-	GroupId                  int              `json:"group_id"`
-	BaseAccessLevel          AccessLevelValue `json:"base_access_level"`
-	AdminCICDVariables       bool             `json:"admin_cicd_variables,omitempty"`
-	AdminMergeRequests       bool             `json:"admin_merge_request,omitempty"`
-	AdminTerraformState      bool             `json:"admin_terraform_state,omitempty"`
-	AdminVulnerability       bool             `json:"admin_vulnerability,omitempty"`
-	ReadCode                 bool             `json:"read_code,omitempty"`
-	ReadDependency           bool             `json:"read_dependency,omitempty"`
-	ReadVulnerability        bool             `json:"read_vulnerability,omitempty"`
-	AdminGroupMembers        bool             `json:"admin_group_member,omitempty"`
-	ManageProjectAccessToken bool             `json:"manage_project_access_tokens,omitempty"`
-	ArchiveProject           bool             `json:"archive_project,omitempty"`
-	RemoveProject            bool             `json:"remove_project,omitempty"`
-	ManageGroupAccesToken    bool             `json:"manage_group_access_tokens,omitempty"`
+	ID                         int              `json:"id"`
+	Name                       string           `json:"name"`
+	Description                string           `json:"description,omitempty"`
+	GroupId                    int              `json:"group_id"`
+	BaseAccessLevel            AccessLevelValue `json:"base_access_level"`
+	AdminCICDVariables         bool             `json:"admin_cicd_variables,omitempty"`
+	AdminComplianceFramework   bool             `json:"admin_compliance_framework,omitempty"`
+	AdminGroupMembers          bool             `json:"admin_group_member,omitempty"`
+	AdminMergeRequests         bool             `json:"admin_merge_request,omitempty"`
+	AdminPushRules             bool             `json:"admin_push_rules,omitempty"`
+	AdminTerraformState        bool             `json:"admin_terraform_state,omitempty"`
+	AdminVulnerability         bool             `json:"admin_vulnerability,omitempty"`
+	AdminWebHook               bool             `json:"admin_web_hook,omitempty"`
+	ArchiveProject             bool             `json:"archive_project,omitempty"`
+	ManageDeployTokens         bool             `json:"manage_deploy_tokens,omitempty"`
+	ManageGroupAccesToken      bool             `json:"manage_group_access_tokens,omitempty"`
+	ManageMergeRequestSettings bool             `json:"manage_merge_request_settings,omitempty"`
+	ManageProjectAccessToken   bool             `json:"manage_project_access_tokens,omitempty"`
+	ManageSecurityPolicyLink   bool             `json:"manage_security_policy_link,omitempty"`
+	ReadCode                   bool             `json:"read_code,omitempty"`
+	ReadRunners                bool             `json:"read_runners,omitempty"`
+	ReadDependency             bool             `json:"read_dependency,omitempty"`
+	ReadVulnerability          bool             `json:"read_vulnerability,omitempty"`
+	RemoveGroup                bool             `json:"remove_group,omitempty"`
+	RemoveProject              bool             `json:"remove_project,omitempty"`
 }
 
 // ListMemberRoles gets a list of member roles for a specified group.
