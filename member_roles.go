10452284305aade2dd004741e501e92499adcac4
Simon Alexander Alsing <aalsing@gmail.com>
2024-08-24 12:56:30 +0200

    Add properties AdminCICDVariables AdminComplianceFramework AdminGroupMembers AdminPushRules AdminTerraformState AdminWebHook ArchiveProject ManageDeployTokens ManageGroupAccesToken ManageMergeRequestSettings ManageProjectAccessToken ManageSecurityPolicyLink ReadRunners RemoveGroup RemoveProject to CreateMemberRoleOptions to align with 17.4.0 spec

--- member_roles.go
+++ member_roles.go
@@ -74,14 +74,29 @@ func (s *MemberRolesService) ListMemberRoles(gid interface{}, options ...Request
 // GitLab API docs:
 // https://docs.gitlab.com/ee/api/member_roles.html#add-a-member-role-to-a-group
 type CreateMemberRoleOptions struct {
-	Name               *string           `url:"name,omitempty" json:"name,omitempty"`
-	BaseAccessLevel    *AccessLevelValue `url:"base_access_level,omitempty" json:"base_access_level,omitempty"`
-	Description        *string           `url:"description,omitempty" json:"description,omitempty"`
-	AdminMergeRequest  *bool             `url:"admin_merge_request,omitempty" json:"admin_merge_request,omitempty"`
-	AdminVulnerability *bool             `url:"admin_vulnerability,omitempty" json:"admin_vulnerability,omitempty"`
-	ReadCode           *bool             `url:"read_code,omitempty" json:"read_code,omitempty"`
-	ReadDependency     *bool             `url:"read_dependency,omitempty" json:"read_dependency,omitempty"`
-	ReadVulnerability  *bool             `url:"read_vulnerability,omitempty" json:"read_vulnerability,omitempty"`
+	Name                       *string           `url:"name,omitempty" json:"name,omitempty"`
+	BaseAccessLevel            *AccessLevelValue `url:"base_access_level,omitempty" json:"base_access_level,omitempty"`
+	Description                *string           `url:"description,omitempty" json:"description,omitempty"`
+	AdminCICDVariables         *bool             `url:"admin_cicd_variables" json:"admin_cicd_variables,omitempty"`
+	AdminComplianceFramework   *bool             `url:"admin_compliance_framework" json:"admin_compliance_framework,omitempty"`
+	AdminGroupMembers          *bool             `url:"admin_group_member" json:"admin_group_member,omitempty"`
+	AdminMergeRequest          *bool             `url:"admin_merge_request,omitempty" json:"admin_merge_request,omitempty"`
+	AdminPushRules             *bool             `url:"admin_push_rules" json:"admin_push_rules,omitempty"`
+	AdminTerraformState        *bool             `url:"admin_terraform_state" json:"admin_terraform_state,omitempty"`
+	AdminVulnerability         *bool             `url:"admin_vulnerability,omitempty" json:"admin_vulnerability,omitempty"`
+	AdminWebHook               *bool             `url:"admin_web_hook" json:"admin_web_hook,omitempty"`
+	ArchiveProject             *bool             `url:"archive_project" json:"archive_project,omitempty"`
+	ManageDeployTokens         *bool             `url:"manage_deploy_tokens" json:"manage_deploy_tokens,omitempty"`
+	ManageGroupAccesToken      *bool             `url:"manage_group_access_tokens" json:"manage_group_access_tokens,omitempty"`
+	ManageMergeRequestSettings *bool             `url:"manage_merge_request_settings" json:"manage_merge_request_settings,omitempty"`
+	ManageProjectAccessToken   *bool             `url:"manage_project_access_tokens" json:"manage_project_access_tokens,omitempty"`
+	ManageSecurityPolicyLink   *bool             `url:"manage_security_policy_link" json:"manage_security_policy_link,omitempty"`
+	ReadCode                   *bool             `url:"read_code,omitempty" json:"read_code,omitempty"`
+	ReadRunners                *bool             `url:"read_runners" json:"read_runners,omitempty"`
+	ReadDependency             *bool             `url:"read_dependency,omitempty" json:"read_dependency,omitempty"`
+	ReadVulnerability          *bool             `url:"read_vulnerability,omitempty" json:"read_vulnerability,omitempty"`
+	RemoveGroup                *bool             `url:"remove_group" json:"remove_group,omitempty"`
+	RemoveProject              *bool             `url:"remove_project" json:"remove_project,omitempty"`
 }
 
 // CreateMemberRole creates a new member role for a specified group.
