0a72e2ccb0ce490904f33675649e0c0dedc5e462
Sander van Harmelen <sander@vanharmelen.nl>
2024-10-14 11:24:34 +0200

    Reorder fields to align with the docs

--- protected_branches.go
+++ protected_branches.go
@@ -54,9 +54,9 @@ type BranchAccessDescription struct {
 	ID                     int              `json:"id"`
 	AccessLevel            AccessLevelValue `json:"access_level"`
 	AccessLevelDescription string           `json:"access_level_description"`
+	DeployKeyID            int              `json:"deploy_key_id"`
 	UserID                 int              `json:"user_id"`
 	GroupID                int              `json:"group_id"`
-	DeployKeyID            int              `json:"deploy_key_id"`
 }
 
 // ListProtectedBranchesOptions represents the available ListProtectedBranches()
