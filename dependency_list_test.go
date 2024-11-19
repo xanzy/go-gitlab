cf43094ff3b4da4cb217ceb187799f66fe026378
Philippe <philippe.a.aviles@lmco.com>
2024-11-18 23:09:14 -0400

    feat: add dependency list client service

---
+++ dependency_list_test.go
@@ -0,0 +1,67 @@
+package gitlab
+
+import (
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateDependencyListExport(t *testing.T) {
+	mux, client := setup(t)
+
+	mux.HandleFunc("/api/v4/pipelines/1234/dependency_list_exports", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		mustWriteHTTPResponse(t, w, "testdata/create_dependency_list_export.json")
+	})
+
+	export, _, err := client.DependencyList.CreateDependencyListExport(1234)
+	require.NoError(t, err)
+
+	want := &DependencyListExport{
+		ID:          5678,
+		HasFinished: false,
+		Self:        "http://gitlab.example.com/api/v4/dependency_list_exports/5678",
+		Download:    "http://gitlab.example.com/api/v4/dependency_list_exports/5678/download",
+	}
+	require.Equal(t, want, export)
+}
+
+func TestGetDependencyListExport(t *testing.T) {
+	mux, client := setup(t)
+
+	mux.HandleFunc("/api/v4/dependency_list_exports/5678", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		mustWriteHTTPResponse(t, w, "testdata/get_dependency_list_export.json")
+	})
+
+	export, _, err := client.DependencyList.GetDependencyListExport(5678)
+	require.NoError(t, err)
+
+	want := &DependencyListExport{
+		ID:          5678,
+		HasFinished: true,
+		Self:        "http://gitlab.example.com/api/v4/dependency_list_exports/5678",
+		Download:    "http://gitlab.example.com/api/v4/dependency_list_exports/5678/download",
+	}
+	require.Equal(t, want, export)
+}
+
+func TestDownloadDependencyListExport(t *testing.T) {
+	mux, client := setup(t)
+
+	mux.HandleFunc("/api/v4/dependency_list_exports/5678/download", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		mustWriteHTTPResponse(t, w, "testdata/download_dependency_list_export.json")
+	})
+
+	sbom, _, err := client.DependencyList.DownloadDependencyListExport(5678)
+	require.NoError(t, err)
+
+	wantBytes, err := os.ReadFile("testdata/download_dependency_list_export.json")
+	require.NoError(t, err)
+	want := string(wantBytes)
+
+	require.Equal(t, want, sbom)
+}
