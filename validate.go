afed20cdac1fb4e96df58901a0f8b7699f87bb48
Jan Wolfensberger <semerun33@gmail.com>
2024-12-03 12:57:14 +0100

    Extend project lint result struct with includes
    The Gitlab API exposes a list of (recursive) includes when linting
    a projects ci configuration. The ProjectLintResult struct representing
    the API response does not reflect this list, so a user of the SDK
    cannot use this information from the API.

--- validate.go
+++ validate.go
@@ -44,10 +44,26 @@ type LintResult struct {
 // GitLab API docs:
 // https://docs.gitlab.com/ee/api/lint.html#validate-a-projects-ci-configuration
 type ProjectLintResult struct {
-	Valid      bool     `json:"valid"`
-	Errors     []string `json:"errors"`
-	Warnings   []string `json:"warnings"`
-	MergedYaml string   `json:"merged_yaml"`
+	Valid      bool      `json:"valid"`
+	Errors     []string  `json:"errors"`
+	Warnings   []string  `json:"warnings"`
+	MergedYaml string    `json:"merged_yaml"`
+	Includes   []Include `json:"includes"`
+}
+
+// Include contains the details about an include block in the .gitlab-ci.yml file.
+// It is used in ProjectLintResult.
+//
+// Reference can be found at the lint API endpoint in the openapi yaml:
+// https://gitlab.com/gitlab-org/gitlab/-/blob/master/doc/api/openapi/openapi_v2.yaml
+type Include struct {
+	Type           string                 `json:"type"`
+	Location       string                 `json:"location"`
+	Blob           string                 `json:"blob"`
+	Raw            string                 `json:"raw"`
+	Extra          map[string]interface{} `json:"extra"`
+	ContextProject string                 `json:"context_project"`
+	ContextSHA     string                 `json:"context_sha"`
 }
 
 // LintOptions represents the available Lint() options.
